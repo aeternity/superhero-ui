69d8b1c616cdf2ed620f971ecfcc0f7fa31aac97
Milen Radkov <milenradkov@me.com>
2020-01-29 13:12:19 +0200

    quickfix: change network to sdk-mainnet instead of aeternal

--- src/components/TipsList.vue
+++ src/components/TipsList.vue
@@ -89,7 +89,7 @@
       },
       data() {
         return {
-          nodeUrl: "https://mainnet.aeternal.io",
+          nodeUrl: "https://sdk-mainnet.aepps.com",
           keypair: Crypto.generateKeyPair(),
           contractCode: CONTRACT_TIP_ANY,
           contractInstance: null,

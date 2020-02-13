402c46d2457db8dd09895e683d728aa98e2ee357
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-02-13 15:32:20 +0200

    Created simple dropdown component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './styles/base.scss';
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { BootstrapVue } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
 

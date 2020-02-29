03ec370f81d276d609b7e7cffb96f2ec9d2d9dea
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-02-29 13:36:28 +0200

    Fixed broken header nav

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -5,17 +5,13 @@
         <img src="../../assets/headerLogo.svg">
       </div>
     </div>
-    <div class="header__body">
+    <div class="header__body container">
       <slot></slot>
     </div>
     <div class="header__right_section">
       <div class="header__buttons">
-        <div>
-          <a class="redeem__tip colorful-buttons secondary" href="">Redeem Tip</a>
-        </div>
-        <div>
-          <a class="send__tip colorful-buttons" href="">Send Tip</a>
-        </div>
+        <a class="redeem__tip colorful-buttons secondary" href="">Redeem Tip</a>
+        <a class="send__tip colorful-buttons" href="">Send Tip</a>
       </div>
     </div>
   </div>
@@ -35,10 +31,10 @@
 .colorful-buttons{
   background-color: $colorful_buttons_background_primary;
   color: $custom_links_color;
-  padding: .75rem 1rem .75rem 1rem;
+  padding: .65rem;
   border-radius: .25rem;
   margin-right: 0;
-  font-size: .65rem;
+  font-size: 1vw;
   font-weight: 600;
   &.secondary{
     background-color: $colorful_buttons_background_secondary;
@@ -47,6 +43,7 @@
   }
 }
 .header{
+  background-color: $background_color;
   position: sticky;
   position: -webkit-sticky;
   top: 0;
@@ -56,7 +53,7 @@
   margin: 0 1rem 0 0;
   min-height: 3rem;
   .header__left_section{
-    width: calc(50% - 17.7rem);
+    // width: calc(50% - 17.7rem);
     display: inline-block;
     text-align: right;
     .content{
@@ -77,16 +74,17 @@
       width: 6rem;
     }
     .header__buttons {
-      display: flex;
+      // display: flex;
     }
   }
 }
 .header__body{
-  width: 33rem;
+  // width: 33rem;
+  width: 100%;
   min-height: 1.5rem;
   display: inline-block;
-  padding-left: .75rem;
-  padding-right: .75rem;
+  // padding-left: .75rem;
+  // padding-right: .75rem;
 }
 
 //Smallest devices Portrait and Landscape
@@ -102,89 +100,89 @@
     .header .header__left_section{display:none}
   }
 
-@media only screen and (max-width: 576px) {
-  .header__body.header__body {
-    width: 100%;
-  }
-}
+// @media only screen and (max-width: 576px) {
+//   .header__body.header__body {
+//     width: 100%;
+//   }
+// }
 
-@media only screen and (max-width: 768px) {
-  .header__body {
-    width: 27rem;
-    padding-left: .6rem;
-    padding-right: .9rem;
-  }
-  .header .header__left_section{
-    width: calc(50% - 13.5rem);
-    img{
-      display: none;
-    }
-  }
-  .header .header__right_section {
-    display: none;
-  }
-}
+// @media only screen and (max-width: 768px) {
+//   .header__body {
+//     width: 27rem;
+//     padding-left: .6rem;
+//     padding-right: .9rem;
+//   }
+//   .header .header__left_section{
+//     width: calc(50% - 13.5rem);
+//     img{
+//       display: none;
+//     }
+//   }
+//   .header .header__right_section {
+//     display: none;
+//   }
+// }
 
 
-@media (min-width: 768px) {
-  .header__body {
-    width: 27rem;
-    padding-left: .75rem;
-    padding-right: .75rem;
-  }
-  .header .header__left_section{
-    width: calc(50% - 13.5rem);
-    padding-right: 0.5rem;
-  }
-  .header .header__right_section{
-    width: calc(50% - 16rem);
-    padding-right: 0.5rem;
-  }
-  .colorful-buttons {
-    font-size: .55rem;
-  }
-}
+// @media (min-width: 768px) {
+//   .header__body {
+//     width: 27rem;
+//     padding-left: .75rem;
+//     padding-right: .75rem;
+//   }
+//   .header .header__left_section{
+//     width: calc(50% - 13.5rem);
+//     padding-right: 0.5rem;
+//   }
+//   .header .header__right_section{
+//     width: calc(50% - 16rem);
+//     padding-right: 0.5rem;
+//   }
+//   .colorful-buttons {
+//     font-size: .55rem;
+//   }
+// }
 
-@media (min-width: 992px) {
-  .header .header__body {
-    width: 30rem;
-  }
-  .header .header__left_section{
-    width: calc(50% - 15.2rem);
-    padding-right: 0.5rem;
-  }
-  .header .header__right_section{
-    width: calc(50% - 16rem);
-    padding-right: 0.5rem;
-  }
-}
+// @media (min-width: 992px) {
+//   .header .header__body {
+//     width: 30rem;
+//   }
+//   .header .header__left_section{
+//     width: calc(50% - 15.2rem);
+//     padding-right: 0.5rem;
+//   }
+//   .header .header__right_section{
+//     width: calc(50% - 16rem);
+//     padding-right: 0.5rem;
+//   }
+// }
 
-@media (min-width: 1200px) {
-  .header .header__body {
-    width: 35rem;
-    padding-left: .75rem;
-    padding-right: .75rem;
-  }
-  .header .header__left_section{
-    width: calc(50% - 17.7rem);
-    padding-right: 0.5rem;
-  }
-  .header .header__right_section{
-    width: calc(50% - 18rem);
-    padding-right: 0.5rem;
-  }
-}
+// @media (min-width: 1200px) {
+//   .header .header__body {
+//     width: 35rem;
+//     padding-left: .75rem;
+//     padding-right: .75rem;
+//   }
+//   .header .header__left_section{
+//     width: calc(50% - 17.7rem);
+//     padding-right: 0.5rem;
+//   }
+//   .header .header__right_section{
+//     width: calc(50% - 18rem);
+//     padding-right: 0.5rem;
+//   }
+// }
 
-@media only screen and (max-width: 768px) {
-  .header .header__body {
-    width: 100%;
-  }
-  .header .header__left_section {
-    display: none;
-  }
-  .header .header__right_section {
-    display: none;
-  }
-}
+// @media only screen and (max-width: 768px) {
+//   .header .header__body {
+//     width: 100%;
+//   }
+//   .header .header__left_section {
+//     display: none;
+//   }
+//   .header .header__right_section {
+//     display: none;
+//   }
+// }
 
 </style>

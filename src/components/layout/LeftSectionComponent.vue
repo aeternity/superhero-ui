74104e61398d51d8302d670444d4591efc608eb1
Philipp Piwo <github@piwo.space>
2020-03-02 17:02:17 +0100

    highlight active side menu

--- src/components/layout/LeftSectionComponent.vue
+++ src/components/layout/LeftSectionComponent.vue
@@ -5,31 +5,35 @@
         <div class="navigation__item home">
           <a @click="goHome()">
             <img class="mr-1" src="../../assets/iconHome.svg">
-            Home
+            <span v-bind:class="{ active: $route.name === 'home' }">Home</span>
           </a>
         </div>
         <div class="navigation__item">
           <a>
-            <img class="mr-1" src="../../assets/iconTrending.svg">Trending
+            <img class="mr-1" src="../../assets/iconTrending.svg">
+            <span v-bind:class="{ active: $route.name === 'trending' }">Trending</span>
           </a>
         </div>
         <div class="navigation__item">
           <a><img class="mr-1" src="../../assets/iconMission.svg">
             <router-link to="/mission">
-              Mission
+              <span v-bind:class="{ active: $route.name === 'mission' }">Mission</span>
             </router-link>
           </a>
         </div>
         <div class="navigation__item" v-if="!isLoggedIn">
           <a><img class="mr-1 avatar" src="../../assets/userAvatar.svg">
             <router-link to="/create-profile">
-              Create Profile
+              <span v-bind:class="{ active: $route.name === 'user-profile' }">Create Profile</span>
             </router-link>
-            </a>
+          </a>
         </div>
-         <div class="navigation__item" v-if="isLoggedIn">
-           <a @click="openMyProfile">
-             <img class="mr-1 avatar" src="../../assets/userAvatar.svg">My Profile</a></div>
+        <div class="navigation__item" v-if="isLoggedIn">
+          <a @click="openMyProfile">
+            <img class="mr-1 avatar" src="../../assets/userAvatar.svg">
+            <span v-bind:class="{ active: $route.name === 'user-profile' }">My Profile</span>
+          </a>
+        </div>
       </div>
       <div class="overview">
         <div class="overview__item">
@@ -100,95 +104,114 @@
 
 <style lang="scss" scoped>
   @import "../../styles/base";
-.app__leftcolumn{
-  color: $standard_font_color;
-  display: inline-block;
-  font-size: .55rem;
-  position: fixed;
-  text-align: right;
-  .content{
-    width: 12.5rem;
-    margin-top: 1rem;
+
+  .app__leftcolumn {
+    color: $standard_font_color;
     display: inline-block;
-    text-transform: uppercase;
-    text-align: left;
-    padding-left: 2rem;
-    .navigation{
-      margin-bottom: 1rem;
-      font-weight: 600;
-      .avatar{
-        width: 1.2rem;
-      }
-    }
-    .navigation__item, .side__button{
-      font-size: .75rem;
-      text-transform: capitalize;
-      margin-bottom: 0.7rem;
-      &:hover{
-        cursor: pointer;
-      }
-    }
-    .side__button{
-      color: $custom_links_color;
-      border: .065rem solid $custom_links_color;
-      border-radius: .25rem;
-      padding: .1rem .5rem .1rem .5rem;
-      text-align: center;
-      &.secondary{
-        color: $secondary_color;
-        border: .065rem solid $secondary_color;
+    font-size: .55rem;
+    position: fixed;
+    text-align: right;
+
+    .content {
+      width: 12.5rem;
+      margin-top: 1rem;
+      display: inline-block;
+      text-transform: uppercase;
+      text-align: left;
+      padding-left: 2rem;
+
+      .navigation {
         margin-bottom: 1rem;
-      }
-    }
-    .overview{
-      margin-top: 2rem;
-      .overview__value{
-        color: $custom_links_color;
-        font-size: 1rem;
-        &.secondary{
-          color: $secondary_color;
+        font-weight: 600;
+
+        .avatar {
+          width: 1.2rem;
         }
       }
-      .overview__label{
-        margin-bottom: .5rem;
-        text-transform: none;
-        color: $light_font_color;
-        &:last-child{margin-bottom: none;}
+
+      .navigation__item, .side__button {
+        font-size: .75rem;
+        text-transform: capitalize;
+        margin-bottom: 0.7rem;
+
+        &:hover {
+          cursor: pointer;
+        }
+
+        .active {
+          color: $custom_links_color;
+        }
+      }
+
+      .side__button {
+        color: $custom_links_color;
+        border: .065rem solid $custom_links_color;
+        border-radius: .25rem;
+        padding: .1rem .5rem .1rem .5rem;
+        text-align: center;
+
+        &.secondary {
+          color: $secondary_color;
+          border: .065rem solid $secondary_color;
+          margin-bottom: 1rem;
+        }
+      }
+
+      .overview {
+        margin-top: 2rem;
+
+        .overview__value {
+          color: $custom_links_color;
+          font-size: 1rem;
+
+          &.secondary {
+            color: $secondary_color;
+          }
+        }
+
+        .overview__label {
+          margin-bottom: .5rem;
+          text-transform: none;
+          color: $light_font_color;
+
+          &:last-child {
+            margin-bottom: none;
+          }
+        }
       }
     }
   }
-}
 
-@media (min-width: 768px) {
-  .app__leftcolumn {
-    .content {
-      width: 12.5rem;
+  @media (min-width: 768px) {
+    .app__leftcolumn {
+      .content {
+        width: 12.5rem;
+      }
     }
   }
-}
 
-@media (min-width: 992px) {
-  .app__leftcolumn {
-    .content {
-      width: 12.5rem;
+  @media (min-width: 992px) {
+    .app__leftcolumn {
+      .content {
+        width: 12.5rem;
+      }
     }
   }
-}
 
-@media (min-width: 1200px) {
-  .app__leftcolumn {
-    .content {
-      width: 12.5rem;
+  @media (min-width: 1200px) {
+    .app__leftcolumn {
+      .content {
+        width: 12.5rem;
+      }
     }
   }
-}
 
-@media only screen and (max-width: 768px) {
-  .app__leftcolumn {
-    .content {
-      width: 10rem;
+  @media only screen and (max-width: 768px) {
+    .app__leftcolumn {
+      .content {
+        width: 10rem;
+      }
     }
   }
-}
 
 </style>

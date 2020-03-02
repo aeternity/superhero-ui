78201f7e62fcef3cad3a87a3595bf245f6799501
Philipp Piwo <github@piwo.space>
2020-03-02 12:59:27 +0100

    move sort function to util, apply in actions

---
+++ src/store/util.js
@@ -0,0 +1,17 @@
+import BigNumber from "bignumber.js";
+
+export const sort = (sortBy, tips) => {
+  switch (sortBy) {
+    case "hot":
+      tips.sort((a, b) => b.score - a.score);
+      break;
+    case "latest":
+      tips.sort((a, b) => b.timestamp - a.timestamp);
+      break;
+    case "highest":
+      tips.sort((a, b) => new BigNumber(b.total_amount).minus(a.total_amount).toNumber());
+      break;
+  }
+
+  return tips;
+};

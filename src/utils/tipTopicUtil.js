960e8c1e0fdd1fbf6cbc3568fa5ce116d60e38c8
Philipp Piwo <github@piwo.space>
2020-03-04 15:30:49 +0100

    score topics

--- src/utils/tipTopicUtil.js
+++ src/utils/tipTopicUtil.js
@@ -1,16 +1,38 @@
 import BigNumber from "bignumber.js";
 
 const getTipTopics = (tips) => {
+  const avgTipScoreWeight = 1.5;
+  const countScoreWeight = 0.8;
 
   const topics = tips.reduce((acc, tip) => {
     if (tip.topics) tip.topics.forEach(topic => {
-      if (topic) acc[topic] = acc[topic] ? new BigNumber(acc[topic]).plus(tip.total_amount).toFixed() : tip.total_amount
+      const score = tip.score ? tip.score : 0;
+
+      if (topic) acc[topic] = acc[topic] ? {
+        amount: new BigNumber(acc[topic].amount).plus(tip.total_amount).toFixed(),
+        totalScore: acc[topic].totalScore + score,
+        count: acc[topic].count + 1
+      } : {
+        amount: tip.total_amount,
+        totalScore: score,
+        count: 1
+      }
     });
 
     return acc;
   }, {});
 
-  return Object.entries(topics).sort((a, b) => new BigNumber(b[1]).minus(a[1]).toNumber());
+  const maxCount = Math.max(...Object.values(topics).map(x => x.count));
+
+  const sortedTopic = Object.entries(topics).map(([topic, data]) => {
+      data.avgScore = data.totalScore / data.count;
+      data.countScore  = data.count / maxCount;
+
+      data.score = data.avgScore * avgTipScoreWeight + data.countScore * countScoreWeight;
+      return [topic, data];
+    }).sort((a, b) => new BigNumber(b[1].score).minus(a[1].score).toNumber());
+
+  return sortedTopic.slice(0, 10);
 };
 
 export default {

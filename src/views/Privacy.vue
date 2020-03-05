2545e0d1971511c4b08df538220485ec6df74ce5
mradkov <milenradkov@me.com>
2020-03-05 05:47:42 +0530

    updated privacy policy and terms

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -14,11 +14,11 @@
 
           <p>
             I. Introduction
-            xyz ​(“​xyz”, “​we​”, “​us​” or “​our​”) is a company which takes your privacy and the security of your information as a ​User​ very seriously.
+            Superhero.com LVC, Dr. Grass Str 12, 9490 Vaduz, Liechtenstein ​(“​Superhero.com”, “​we​”, “​us​” or “​our​”) is a company which takes your privacy and the security of your information as a ​User​ very seriously.
           </p>
 
           <p>
-            This ​Privacy Policy (“​Policy​”) is applicable for the ​xyz ​created and operated ​Website http://youdonotneedacapetobeahero.com/ (​the ​“Website”), ​reflecting users’ donations and comments to content gives the possibility for other users to actively donate and comment on the news available on the newsfeed of the Website​.
+            This ​Privacy Policy (“​Policy​”) is applicable for the ​Superhero.com ​created and operated ​Website http://youdonotneedacapetobeahero.com/ (​the ​“Website”), ​reflecting users’ donations and comments to content gives the possibility for other users to actively donate and comment on the news available on the newsfeed of the Website​.
           </p>
           <p>
             If you have any questions regarding this ​Policy​, please contact us at superherowallet@protonmail.com​. If you do not agree to the terms of this Policy, please do not use ​the Website​.
@@ -37,8 +37,8 @@
           </p>
           <p>
             III. Personal data collection
-            The personal data ​xyz ​collects is minimized completely, selecting only those types of information, which are adequate, relevant and limited to what is necessary for the use of ​the Website​.
-            xyz ​collects two main groups of personal data according to their purpose:
+            The personal data ​Superhero.com ​collects is minimized completely, selecting only those types of information, which are adequate, relevant and limited to what is necessary for the use of ​the Website​.
+            Superhero.com ​collects two main groups of personal data according to their purpose:
             3.1. Personal data in relation to indirect identifiers
             The donations and comments are conducted via the Extension Superherowallet. The Superhero wallet is a blockchain based application on which an account address is generated for user identification. Once the donation and comment are processed, they appear on the Website with a hyperlink forwarding to the website the User has tipped. ​Furthermore, other ​Users on the Website​ can additionally donate and comment on news added to the ​Website​.
             Therefore, the account address will appear on the ​Website​. The account addresses can be treated as indirect identifiers. An indirect identifier is a value which cannot be used alone to identify a data subject, however, could be identifying if combined with other indirect identifiers.
@@ -46,13 +46,13 @@
             3.2. ​Personal data in relation to User support and communication
             During the use of our Website, users may decide to contact the team in relation to website support, troubleshoot problems, questions, etc. The collection of the personal data is initiated by Users and they decide what type of personal data to include in the communication.
             Therefore, the legal basis for processing is that the User has given consent to the processing of their personal data for one or more specific purposes.
-            The retention period is until the questions have been answered, assistance from the team has been provided, the problems have been solved or in any other case, until withdrawal of consent. xyz may retent personal information for additional period of time, if it is required by law or for crime/breach/ infringement prevention and safety.
+            The retention period is until the questions have been answered, assistance from the team has been provided, the problems have been solved or in any other case, until withdrawal of consent. Superhero.com may retent personal information for additional period of time, if it is required by law or for crime/breach/ infringement prevention and safety.
           </p>
           <p>
             IV. Your rights:
             YOU​ have the right to:
-            ▪ Request from ​xyz ​access to the personal data that we hold about ​You​ in a portable format.
-            ▪ Request from ​xyz ​correction of any collected personal data. The information may include only
+            ▪ Request from ​Superhero.com ​access to the personal data that we hold about ​You​ in a portable format.
+            ▪ Request from ​Superhero.com ​correction of any collected personal data. The information may include only
             your personal data collection.
             ▪ Receive a copy of your personal data in electronic format.
             ▪ “The right to be forgotten”- ​You have the right to request the deletion of your personal data at
@@ -68,18 +68,18 @@
             V. Sharing of Personal Information with Third Parties- Recipients
             ▪ We may disclose ​USERS’ identifiable personal information to third parties under special circumstances:
 
-            (i) to comply with a legal requirement, judicial proceeding, court order, or legal process served on ​xyz ​or its affiliates,
+            (i) to comply with a legal requirement, judicial proceeding, court order, or legal process served on ​Superhero.com ​or its affiliates,
             (ii) to investigate a possible crime, such as fraud or identity theft;
             (iii) to Public Authorities in accordance with their legal obligation for the exercise of their official mission, such as tax and customs authorities, financial investigation units, independent administrative authorities, or financial marker authorities responsible for supervision of securities markets;
-            (iv) in connection with the sale, purchase, business transition under any form, merger, dissolution, reorganization, liquidation of ​xyz ​; ​We may have to disclose the information ​You​ have given us to the successor, who is part of the transition,
-            (v) when ​We believe it is necessary to protect the rights, property, or safety of ​xyz ​or other persons, or
-            (vi) as otherwise required or permitted by law, including any contractual obligations of xyz ​.
+            (iv) in connection with the sale, purchase, business transition under any form, merger, dissolution, reorganization, liquidation of ​Superhero.com ​; ​We may have to disclose the information ​You​ have given us to the successor, who is part of the transition,
+            (v) when ​We believe it is necessary to protect the rights, property, or safety of ​Superhero.com ​or other persons, or
+            (vi) as otherwise required or permitted by law, including any contractual obligations of Superhero.com ​.
           </p>
           <p>
             VI. Children and Privacy
-            ▪ xyz ​does not process information of persons under the age of obtaining full legal capacity
+            ▪ Superhero.com ​does not process information of persons under the age of obtaining full legal capacity
              (in most countries 18 years old) due to the character of the ​Website​ and the ​Wallet​.
-            ▪ xyz ​will immediately delete any personal data referring to persons under the age of full
+            ▪ Superhero.com ​will immediately delete any personal data referring to persons under the age of full
               legal capacity.
             ▪ We are not liable in any way if ​YOU provide false information about your age or use the
               Website before ​YOU​ have turned the above-mentioned age.
@@ -89,8 +89,8 @@
             In case of breach, ​We will undertake every possible action according to the applicable legislation in an appropriate and timely manner, to avoid any material or non-material damage to ​Users and to protect the personal data of ​Users​.
           </p>
           <p>
-            VIII. How Do I contact xyz ?
-            To contact us with your questions or comments regarding this Policy or the information collection and dissemination practices of ​xyz ​relevant to your use of the Website, please email us at superherowallet@protonmail.com​.
+            VIII. How Do I contact Superhero.com ?
+            To contact us with your questions or comments regarding this Policy or the information collection and dissemination practices of ​Superhero.com ​relevant to your use of the Website, please email us at superherowallet@protonmail.com​.
           </p>
         </div>
       </div>

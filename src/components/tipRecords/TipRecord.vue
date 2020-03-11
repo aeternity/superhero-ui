0b073e5cc5bc124d066388c16f5bf5c6b389fd3a
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-03-11 18:11:03 +0200

    Issue 114: Show retip section even without values

--- src/components/tipRecords/TipRecord.vue
+++ src/components/tipRecords/TipRecord.vue
@@ -39,7 +39,7 @@
               {{ tip.amount_ae }} <span class="ae">AE</span>
             </span>
             <fiat-value :amount="tip.total_amount"></fiat-value>
-            <span class="ml-4" v-if="tip.retip_amount_ae !== '0'" v-on:click.stop>
+            <span class="ml-4" v-on:click.stop>
               <retip :tipid="tip.id" :retip-icon="true"/>
               {{ tip.retip_amount_ae }} <span class="ae">AE</span>
               <fiat-value :amount="tip.retip_amount_ae"></fiat-value>

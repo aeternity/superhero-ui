7f5f4c06447d5fef16c7147be14ab3566588228b
keno dressel <aeternity@keno.digital>
2020-03-20 12:13:59 +0100

    removes local variable

--- src/utils/backend.js
+++ src/utils/backend.js
@@ -2,46 +2,38 @@ import { wrapTry } from './util';
 import { BACKEND_URL, MAINNET_URL } from '../config/constants';
 
 export default class Backend {
-  BACKEND_URL;
 
-  MAINNET_URL;
+  tipOrder = async () => wrapTry(async () => fetch(`${BACKEND_URL}/tiporder`));
 
-  constructor() {
-    this.BACKEND_URL = BACKEND_URL;
-    this.MAINNET_URL = MAINNET_URL;
-  }
+  tipPreview = async () => wrapTry(async () => fetch(`${BACKEND_URL}/linkpreview`));
 
-  tipOrder = async () => wrapTry(async () => fetch(`${this.BACKEND_URL}/tiporder`));
+  getLangTips = async (lang = 'en') => wrapTry(async () => fetch(`${BACKEND_URL}/language/${lang}`));
 
-  tipPreview = async () => wrapTry(async () => fetch(`${this.BACKEND_URL}/linkpreview`));
+  getTipComments = async (tipId) => wrapTry(async () => fetch(`${BACKEND_URL}/comment/api/tip/${encodeURIComponent(tipId)}`));
 
-  getLangTips = async (lang = 'en') => wrapTry(async () => fetch(`${this.BACKEND_URL}/language/${lang}`));
-
-  getTipComments = async (tipId) => wrapTry(async () => fetch(`${this.BACKEND_URL}/comment/api/tip/${encodeURIComponent(tipId)}`));
-
-  sendTipComment = async (postParam) => wrapTry(async () => fetch(`${this.BACKEND_URL}/comment/api/`, {
+  sendTipComment = async (postParam) => wrapTry(async () => fetch(`${BACKEND_URL}/comment/api/`, {
     method: 'post',
     body: JSON.stringify(postParam),
     headers: { 'Content-Type': 'application/json' },
   }));
 
-  getAllComments = async () => wrapTry(async () => fetch(`${this.BACKEND_URL}/comment/api/`));
+  getAllComments = async () => wrapTry(async () => fetch(`${BACKEND_URL}/comment/api/`));
 
-  getProfile = async (address) => wrapTry(async () => fetch(`${this.BACKEND_URL}/profile/${address}`));
+  getProfile = async (address) => wrapTry(async () => fetch(`${BACKEND_URL}/profile/${address}`));
 
-  sendProfileData = async (postParam) => wrapTry(async () => fetch(`${this.BACKEND_URL}/profile`, {
+  sendProfileData = async (postParam) => wrapTry(async () => fetch(`${BACKEND_URL}/profile`, {
     method: 'post',
     body: JSON.stringify(postParam),
     headers: { 'Content-Type': 'application/json' },
   }));
 
-  getProfileImage = async (address) => wrapTry(async () => fetch(`${this.BACKEND_URL}/profile/image/${address}`));
+  getProfileImage = async (address) => wrapTry(async () => fetch(`${BACKEND_URL}/profile/image/${address}`));
 
-  getStats = async () => wrapTry(async () => fetch(`${this.BACKEND_URL}/static/stats/`));
+  getStats = async () => wrapTry(async () => fetch(`${BACKEND_URL}/static/stats/`));
 
-  getChainNameFromAddress = async () => wrapTry(async () => fetch(`${this.MAINNET_URL}/middleware/names/active`));
+  getChainNameFromAddress = async () => wrapTry(async () => fetch(`${MAINNET_URL}/middleware/names/active`));
 
-  getCommentCounts = async () => wrapTry(async () => fetch(`${this.BACKEND_URL}/comment/count/tips/`));
+  getCommentCounts = async () => wrapTry(async () => fetch(`${BACKEND_URL}/comment/count/tips/`));
 
-  getCommentCountForAddress = async (address) => wrapTry(async () => fetch(`${this.BACKEND_URL}/comment/count/author/${address}`));
+  getCommentCountForAddress = async (address) => wrapTry(async () => fetch(`${BACKEND_URL}/comment/count/author/${address}`));
 }

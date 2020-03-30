177572d934c0594032d1557f58b18e1f60da79a4
Philipp Piwo <github@piwo.space>
2020-03-30 18:15:06 +0200

    Added testnet backend and oracle contract

--- src/utils/aeternity.js
+++ src/utils/aeternity.js
@@ -26,11 +26,7 @@ aeternity.initProvider = async () => {
   // TESTING
   if (typeof Cypress !== 'undefined') {
     aeternity.contractAddress = 'ct_2GRP3xp7KWrKtZSnYfdcLnreRWrntWf5aTsxtLqpBHp71EFc3i';
-    aeternity.oracleContract = {
-      methods: {
-        get_state: async () => ({ success_claimed_urls: [] }),
-      },
-    };
+    aeternity.oracleContractAddress = 'ct_gAmsWu28jMniDJrdDnZ3FobCTZNVg1DQRSyeQF95Hux6rtdG';
     aeternity.client = await Universal({
       compilerUrl: 'https://latest.compiler.aepps.com',
       nodes: [{ name: 'testnet', instance: await Node({ url: 'https://sdk-testnet.aepps.com', internalUrl: 'https://sdk-testnet.aepps.com' }) }],

de29fea3cf55cfd73bff570b66b4847a139c37a0
Valentin Angelov <43376240+vmangelovv@users.noreply.github.com>
2020-04-02 19:10:23 +0300

    Update Maintenance.vue

--- src/views/Maintenance.vue
+++ src/views/Maintenance.vue
@@ -1,21 +1,17 @@
 <template>
-  <div>
-    <div class="maintenance container wrapper mt-2">
-      <div class="p-5 content text-center">
-        <div v-if="view === 'recharge'">
-          <div class="recharge">
-            <h1>We're recharging our powers.</h1>
-            <div class="gif" />
-            <h2>We'll be up, up and away in no time.</h2>
-          </div>
-        </div>
-        <div v-if="view === 'cleaning'">
-          <div class="cleaning">
-            <h1>Cleaning up the HQ.</h1>
-            <div class="gif2" />
-            <h2>We'll be flying again in a few minutes.</h2>
-          </div>
-        </div>
+  <div class="maintenance container">
+    <div v-if="view === 'recharge'">
+      <div class="recharge">
+        <h1>We're recharging our powers.</h1>
+        <img class="maintenance--image" src="../assets/recharge.gif">
+        <h2>We'll be up, up and away in no time.</h2>
+      </div>
+    </div>
+    <div v-if="view === 'cleaning'">
+      <div class="cleaning">
+        <h1>Cleaning up the HQ.</h1>
+        <img class="maintenance--image" src="../assets/ShieldFill.svg">
+        <h2>We'll be flying again in a few minutes.</h2>
       </div>
     </div>
   </div>
@@ -35,55 +31,42 @@ export default {
 
 
 <style lang="scss" scoped>
-.recharge {
+.maintenance {
+  margin: 4rem auto;
+  padding: 1rem;
   h1 {
     font-size: 55px;
     font-weight: bold;
-    color: white;
     text-align: center;
     line-height: 80px;
     margin: 50px 0;
   }
-  .gif {
-    background: url('../assets/recharge.gif') center center;
-    display: block;
-    height: 190px;
-    width: 208px;
-    margin: 0 auto;
-    position: relative;
-  }
   h2 {
     font-size: 35px !important;
     line-height: 55px;
-    color: #3290E2;
     text-align: center;
     margin: 50px 0;
+  }
+  .maintenance--image {
+    display: block;
+    margin: 0 auto;
+  }
+}
+.recharge {
+  h1 {
+    color: white;
+  }
+  h2 {
+    color: #3290E2;
   }
 }
 
 .cleaning {
   h1 {
-    font-size: 55px;
-    font-weight: bold;
     color: #3290E2;
-    text-align: center;
-    line-height: 80px;
-    margin: 50px 0;
-  }
-  .gif2 {
-    background: url('../assets/ShieldFill.svg') center center;
-    display: block;
-    height: 220px;
-    width: 255px;
-    margin: 0 auto;
-    position: relative;
   }
   h2 {
-    font-size: 35px !important;
-    line-height: 55px;
     color: white;
-    text-align: center;
-    margin: 50px 0;
   }
 }
 </style>

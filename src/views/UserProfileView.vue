686fe6e17fa6ec9bbe202a0ac082520f80c83d35
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-04-06 16:18:52 +0300

    Changed explorer link

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -285,7 +285,6 @@ import FiatValue from '../components/FiatValue.vue';
 import AeAmount from '../components/AeAmount.vue';
 import Loading from '../components/Loading.vue';
 import defaultAvatar from '../assets/userAvatar.svg';
-import { MIDDLEWARE_URL } from '../config/constants';
 import TipsPagination from '../components/TipsPagination.vue';
 
 export default {
@@ -305,7 +304,7 @@ export default {
   },
   data() {
     return {
-      explorerUrl: `${MIDDLEWARE_URL}account/transactions/`,
+      explorerUrl: 'https://mainnet.aeternal.io/account/transactions/',
       tip: this.tipData,
       showLoading: false,
       comments: [],

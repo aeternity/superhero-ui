fd5d8568f12de2f1a94d50dab455256d08d0e141
stanislav-slavov <stanislavslavov90@gmail.com>
2020-04-10 18:00:33 +0300

    Fixed issue with url params
    Co-Authored-By: Milen Radkov <milenradkov@me.com>

--- src/router.js
+++ src/router.js
@@ -36,7 +36,7 @@ const routes = [
     meta: { title: 'Home' },
   },
   {
-    path: '/tip/:id/comment/:id',
+    path: '/tip/:tipId/comment/:id',
     name: 'comment',
     component: SingleCommentView,
     meta: {

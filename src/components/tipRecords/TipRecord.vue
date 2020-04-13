1d8ed38303025abf5ae0659280c3e690834d11ea
Milen Radkov <milenradkov@me.com>
2020-04-13 08:49:24 +0300

    add embedly previews

--- src/components/tipRecords/TipRecord.vue
+++ src/components/tipRecords/TipRecord.vue
@@ -30,7 +30,7 @@
         </div>
       </div>
       <div
-        v-if="isPreviewToBeVisualized(tip)"
+        v-if="isPreviewToBeVisualized(tip) && !embedlyPreview(tip)"
         class="tip__article"
       >
         <a
@@ -64,9 +64,7 @@
                 :title="`Initial tip`"
                 @click.prevent
               >
-                <retip
-                  :tip="tip"
-                />
+                <retip :tip="tip" />
               </div>
             </div>
             <img
@@ -85,7 +83,12 @@
         <a
           :href="tip.url"
           :title="tip.url"
-          class="text-ellipsis"
+          class="embedly-card text-ellipsis"
+          data-card-theme="dark"
+          data-card-controls="0"
+          data-card-via=""
+          data-card-recommend="0"
+          data-card-key="57b5154650e74bf19e4f106468d90770"
         >{{ tip.url }}</a>
       </div>
       <div
@@ -103,9 +106,7 @@
             class="tip__amount"
             @click.stop
           >
-            <TipControl
-              :tip="tip"
-            />
+            <TipControl :tip="tip" />
           </div>
           <div
             class="tip__comments"
@@ -164,7 +165,9 @@ export default {
       return this.tip.preview.title ? this.tip.preview.title : '';
     },
     tipPreviewImage() {
-      return this.isPreviewToBeVisualized(this.tip) && this.tip.preview.image !== null ? Backend.getTipPreviewUrl(this.tip.preview.image) : '';
+      return this.isPreviewToBeVisualized(this.tip) && this.tip.preview.image !== null
+        ? Backend.getTipPreviewUrl(this.tip.preview.image)
+        : '';
     },
   },
   methods: {
@@ -172,13 +175,22 @@ export default {
       const userImage = Backend.getProfileImageUrl(address);
       return userImage || this.defaultAvatar;
     },
+    embedlyPreview(tip) {
+      return (
+        tip.url.indexOf('youtube') > -1
+        || tip.url.indexOf('youtu.be') > -1
+        || tip.url.indexOf('twitter') > -1
+        || tip.url.indexOf('facebook') > -1
+      );
+    },
     isPreviewToBeVisualized(tip) {
-      return typeof tip !== 'undefined' && tip !== null
+      return (
+        typeof tip !== 'undefined'
+        && tip !== null
         && typeof tip.preview !== 'undefined'
-        && (
-          (tip.preview.description !== null && tip.preview.description.length > 0)
-          || (tip.preview.title !== null && tip.preview.title.length > 0)
-        );
+        && ((tip.preview.description !== null && tip.preview.description.length > 0)
+          || (tip.preview.title !== null && tip.preview.title.length > 0))
+      );
     },
     goToTip(id) {
       this.$router.push({
@@ -193,392 +205,390 @@ export default {
 </script>
 
 <style lang="scss">
-  .tip__record {
-    background-color: $light_color;
-    margin: 0 0 0.15rem 0;
+.tip__record {
+  background-color: $light_color;
+  margin: 0 0 0.15rem 0;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  .ae-amount {
+    color: $standard_font_color;
+    font-size: 0.8rem;
+  }
+
+  .currency-value {
+    color: $light_font_color;
+    margin-left: 0.1rem;
+    font-size: 0.7rem;
+  }
+}
+
+.tip__body {
+  padding-top: 1rem;
+  width: 100%;
+}
+
+.tip__author {
+  align-items: center;
+  color: $light_font_color;
+  display: flex;
+  font-size: 0.8rem;
+  justify-content: space-between;
+  padding: 0 1rem 0.9rem 1rem;
+
+  .tip__date {
+    display: inline-block;
+    font-size: 0.6rem;
+    text-align: right;
+  }
+
+  .address {
+    font-size: 0.65rem;
+  }
+
+  .address,
+  .chain__name {
+    display: inline-block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 100%;
+    word-break: break-all;
+  }
+
+  img,
+  svg {
+    border-radius: 50%;
+    flex-shrink: 0;
+    height: 2rem;
+    margin-right: 0.25rem;
+    object-fit: cover;
+    width: 2rem;
+  }
+
+  a {
+    color: $light_font_color;
+    display: flex;
+    margin-right: 1rem;
+    overflow: hidden;
 
     &:hover {
-      cursor: pointer;
-    }
-
-    .ae-amount {
-      color: $standard_font_color;
-      font-size: 0.8rem;
-    }
-
-    .currency-value {
-      color: $light_font_color;
-      margin-left: 0.1rem;
-      font-size: 0.7rem;
-    }
-  }
-
-  .tip__body {
-    padding-top: 1rem;
-    width: 100%;
-  }
-
-  .tip__author {
-    align-items: center;
-    color: $light_font_color;
-    display: flex;
-    font-size: 0.8rem;
-    justify-content: space-between;
-    padding: 0 1rem 0.9rem 1rem;
-
-    .tip__date {
-      display: inline-block;
-      font-size: 0.6rem;
-      text-align: right;
-    }
-
-    .address {
-      font-size: 0.65rem;
-    }
-
-    .address,
-    .chain__name {
-      display: inline-block;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      width: 100%;
-      word-break: break-all;
-    }
-
-    img,
-    svg {
-      border-radius: 50%;
-      flex-shrink: 0;
-      height: 2rem;
-      margin-right: 0.25rem;
-      object-fit: cover;
-      width: 2rem;
-    }
-
-    a {
-      color: $light_font_color;
-      display: flex;
-      margin-right: 1rem;
-      overflow: hidden;
-
-      &:hover {
-        filter: brightness(1.3);
-      }
-    }
-
-    .chain__name {
-      color: #fff;
-    }
-
-    .tip__author_name {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      overflow: hidden;
-      width: 100%;
-    }
-  }
-
-  .tip__note {
-    @include truncate-overflow-mx(4);
-
-    color: $tip_note_color;
-    font-size: 0.85rem;
-    line-height: 1.1rem;
-    margin-bottom: 0.8rem;
-    margin-top: 0.85rem;
-    padding-left: 1rem;
-
-    .title .topic {
-      color: $standard_font_color;
-
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-  }
-
-  .retip__icon {
-    height: 1rem;
-    margin-right: 0.2rem;
-    vertical-align: top;
-    padding: 0.1rem 0;
-    width: 1rem;
-  }
-
-  .comment__icon {
-    height: 1rem;
-    margin-right: 0.2rem;
-    vertical-align: top;
-    width: 1rem;
-  }
-
-  .tip__footer {
-    border-bottom-left-radius: 0.25rem;
-    border-bottom-right-radius: 0.25rem;
-    color: $light_font_color;
-    font-size: 0.8rem;
-    padding: 0 1rem 0.75rem;
-  }
-
-  .tip__footer_wrapper {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  // separator
-  .tip__footer_wrapper::after {
-    content: '';
-    flex-basis: 1rem;
-    height: 1rem;
-    order: 3;
-  }
-
-  .tip__comments,
-  .tip__amount {
-    align-items: center;
-    display: flex;
-    flex: 0 0 auto;
-    height: 1rem;
-    margin-right: 1rem;
-    position: relative;
-
-    &.tip__comments--hascomments {
-      color: #fff;
-    }
-  }
-
-  .tip__comments {
-    margin-left: 20%;
-    cursor: pointer;
-    order: 4;
-
-    img {
-      height: 1rem;
-    }
-
-    &:hover img {
       filter: brightness(1.3);
     }
   }
 
-  .tip__url {
-    margin-left: 1rem;
-    margin-right: 1rem;
+  .chain__name {
+    color: #fff;
+  }
 
-    a {
-      font-size: 0.75rem;
+  .tip__author_name {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    overflow: hidden;
+    width: 100%;
+  }
+}
+
+.tip__note {
+  @include truncate-overflow-mx(4);
+
+  color: $tip_note_color;
+  font-size: 0.85rem;
+  line-height: 1.1rem;
+  margin-bottom: 0.8rem;
+  margin-top: 0.85rem;
+  padding-left: 1rem;
+
+  .title .topic {
+    color: $standard_font_color;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+
+.retip__icon {
+  height: 1rem;
+  margin-right: 0.2rem;
+  vertical-align: top;
+  padding: 0.1rem 0;
+  width: 1rem;
+}
+
+.comment__icon {
+  height: 1rem;
+  margin-right: 0.2rem;
+  vertical-align: top;
+  width: 1rem;
+}
+
+.tip__footer {
+  border-bottom-left-radius: 0.25rem;
+  border-bottom-right-radius: 0.25rem;
+  color: $light_font_color;
+  font-size: 0.8rem;
+  padding: 0 1rem 0.75rem;
+}
+
+.tip__footer_wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+// separator
+.tip__footer_wrapper::after {
+  content: '';
+  flex-basis: 1rem;
+  height: 1rem;
+  order: 3;
+}
+
+.tip__comments,
+.tip__amount {
+  align-items: center;
+  display: flex;
+  flex: 0 0 auto;
+  height: 1rem;
+  margin-right: 1rem;
+  position: relative;
+
+  &.tip__comments--hascomments {
+    color: #fff;
+  }
+}
+
+.tip__comments {
+  margin-left: 20%;
+  cursor: pointer;
+  order: 4;
+
+  img {
+    height: 1rem;
+  }
+
+  &:hover img {
+    filter: brightness(1.3);
+  }
+}
+
+.tip__url {
+  margin-left: 1rem;
+  margin-right: 1rem;
+
+  a {
+    font-size: 0.75rem;
+    display: block;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+
+.tip__article {
+  background-color: $thumbnail_background_color;
+  background-image: url('../../assets/defaultImg.svg');
+  background-position: 95% center;
+  background-repeat: no-repeat;
+  background-size: 30%;
+  border-radius: 0.5rem;
+  font-size: 0.75rem;
+  height: 10.5rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  min-height: 5.9rem;
+  padding: 0;
+  position: relative;
+
+  .preview__image {
+    background-color: $thumbnail_background_color;
+    height: 10.5rem;
+    object-fit: cover;
+  }
+
+  .tip__article__content {
+    color: #babac0;
+    font-size: 0.75rem;
+    height: 10.5rem;
+    line-height: 1.1rem;
+    padding: 0.85rem 1rem 0.8rem 1rem;
+
+    .title {
       display: block;
+      font-size: 0.8rem;
+      font-weight: 500;
+      margin-bottom: 0.15rem;
+      color: $tip_note_color;
+    }
 
-      &:hover {
-        text-decoration: underline;
-      }
+    .description {
+      @include truncate-overflow-mx(4);
+
+      margin-bottom: 0.35rem;
+      color: $preview_description_font_color;
     }
   }
 
-  .tip__article {
-    background-color: $thumbnail_background_color;
-    background-image: url("../../assets/defaultImg.svg");
-    background-position: 95% center;
-    background-repeat: no-repeat;
-    background-size: 30%;
-    border-radius: 0.5rem;
-    font-size: 0.75rem;
-    height: 10.5rem;
-    margin-left: 1rem;
-    margin-right: 1rem;
-    min-height: 5.9rem;
-    padding: 0;
-    position: relative;
+  .tip__article--hasresults {
+    display: flex;
 
     .preview__image {
-      background-color: $thumbnail_background_color;
-      height: 10.5rem;
-      object-fit: cover;
+      flex: 0 0 35%;
+      max-width: 35%;
+      min-width: 35%;
+      width: 35%;
     }
 
     .tip__article__content {
-      color: #babac0;
-      font-size: 0.75rem;
-      height: 10.5rem;
-      line-height: 1.1rem;
-      padding: 0.85rem 1rem 0.8rem 1rem;
+      flex: 0 0 65%;
+      max-width: 65%;
+      min-width: 65%;
+      width: 65%;
+    }
+  }
 
-      .title {
-        display: block;
-        font-size: 0.8rem;
-        font-weight: 500;
-        margin-bottom: 0.15rem;
-        color: $tip_note_color;
-      }
+  .site__url {
+    align-items: center;
+    color: $light_font_color;
+    display: flex;
+    font-weight: 500;
+    margin-bottom: 0.45rem;
 
-      .description {
-        @include truncate-overflow-mx(4);
+    img {
+      width: 1rem;
+      height: 1rem;
+      margin-right: 0.335rem;
+      padding: 0.135rem 0;
+    }
+  }
 
-        margin-bottom: 0.35rem;
-        color: $preview_description_font_color;
-      }
+  &:hover {
+    background-color: $thumbnail_background_color_alt;
+    cursor: pointer;
+
+    .preview__image {
+      background-color: $thumbnail_background_color_alt;
     }
 
-    .tip__article--hasresults {
-      display: flex;
+    .site__url {
+      text-decoration: underline;
+    }
 
-      .preview__image {
-        flex: 0 0 35%;
-        max-width: 35%;
-        min-width: 35%;
-        width: 35%;
-      }
+    .tip__article__content {
+      color: #c6c6cc;
+    }
+  }
+}
 
-      .tip__article__content {
-        flex: 0 0 65%;
-        max-width: 65%;
-        min-width: 65%;
-        width: 65%;
+@media only screen and (max-width: 1024px) {
+  .tip__record {
+    position: relative;
+
+    .tip__article {
+      min-height: 2rem;
+    }
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .tip__amount:nth-child(2) .retip__container {
+    left: -50%;
+    right: -50%;
+  }
+
+  .tip__note {
+    font-size: 0.75rem;
+  }
+
+  .tip__article {
+    .tip__article__content {
+      font-size: 0.75rem;
+    }
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .tip__note {
+    font-size: 0.75rem;
+  }
+
+  .tip__footer .tip__amount img {
+    width: 0.7rem;
+  }
+
+  .tip__article {
+    .tip__article__content {
+      font-size: 0.65rem;
+    }
+  }
+}
+
+//Smallest devices Portrait and Landscape
+@media only screen and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+  .tip__body {
+    padding: 0;
+  }
+
+  .tip__record {
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 0.5rem 0.5rem 0.5rem;
+    position: relative;
+  }
+
+  .tip__article {
+    margin-left: 0;
+    max-width: calc(100% + 1rem);
+    width: calc(100% + 1rem);
+
+    .tip__article__content {
+      line-height: 1.1rem;
+
+      .description {
+        @include truncate-overflow-mx(5);
       }
     }
 
     .site__url {
-      align-items: center;
-      color: $light_font_color;
-      display: flex;
-      font-weight: 500;
-      margin-bottom: 0.45rem;
+      text-decoration: underline;
+    }
+  }
 
+  .tip__author {
+    font-size: 0.6rem;
+    padding-left: 0;
+    padding-right: 0;
+
+    img {
+      height: 1.5rem;
+      width: 1.5rem;
+    }
+
+    .address {
+      font-size: 0.55rem;
+    }
+  }
+
+  .tip__note {
+    padding: 0;
+  }
+
+  .tip__footer {
+    font-size: 0.65rem;
+    padding: 0;
+
+    .tip__amount {
       img {
         width: 1rem;
-        height: 1rem;
-        margin-right: 0.335rem;
-        padding: 0.135rem 0;
-      }
-    }
-
-    &:hover {
-      background-color: $thumbnail_background_color_alt;
-      cursor: pointer;
-
-      .preview__image {
-        background-color: $thumbnail_background_color_alt;
-      }
-
-      .site__url {
-        text-decoration: underline;
-      }
-
-      .tip__article__content {
-        color: #c6c6cc;
       }
     }
   }
 
-  @media only screen and (max-width: 1024px) {
-    .tip__record {
-      position: relative;
-
-      .tip__article {
-        min-height: 2rem;
-      }
-    }
+  .tip__url {
+    margin: 0 0 0.4rem 0;
   }
-
-  @media only screen and (max-width: 768px) {
-    .tip__amount:nth-child(2) .retip__container {
-      left: -50%;
-      right: -50%;
-    }
-
-    .tip__note {
-      font-size: 0.75rem;
-    }
-
-    .tip__article {
-      .tip__article__content {
-        font-size: 0.75rem;
-      }
-    }
-  }
-
-  @media only screen and (max-width: 600px) {
-    .tip__note {
-      font-size: 0.75rem;
-    }
-
-    .tip__footer .tip__amount img {
-      width: 0.7rem;
-    }
-
-    .tip__article {
-      .tip__article__content {
-        font-size: 0.65rem;
-      }
-    }
-  }
-
-  //Smallest devices Portrait and Landscape
-  @media only screen
-    and (max-device-width: 480px)
-    and (-webkit-min-device-pixel-ratio: 2) {
-    .tip__body {
-      padding: 0;
-    }
-
-    .tip__record {
-      margin-bottom: 0.5rem;
-      padding: 0.5rem 0.5rem 0.5rem 0.5rem;
-      position: relative;
-    }
-
-    .tip__article {
-      margin-left: 0;
-      max-width: calc(100% + 1rem);
-      width: calc(100% + 1rem);
-
-      .tip__article__content {
-        line-height: 1.1rem;
-
-        .description {
-          @include truncate-overflow-mx(5);
-        }
-      }
-
-      .site__url {
-        text-decoration: underline;
-      }
-    }
-
-    .tip__author {
-      font-size: 0.6rem;
-      padding-left: 0;
-      padding-right: 0;
-
-      img {
-        height: 1.5rem;
-        width: 1.5rem;
-      }
-
-      .address {
-        font-size: 0.55rem;
-      }
-    }
-
-    .tip__note {
-      padding: 0;
-    }
-
-    .tip__footer {
-      font-size: 0.65rem;
-      padding: 0;
-
-      .tip__amount {
-        img {
-          width: 1rem;
-        }
-      }
-    }
-
-    .tip__url {
-      margin: 0 0 0.4rem 0;
-    }
-  }
+}
 </style>

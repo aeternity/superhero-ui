4e77237a46636a8e6b564098059686444d12c32e
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-04-15 19:29:55 +1000

    Extract AeAmountFiat component

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -176,13 +176,10 @@
               <div class="stat-title">
                 Total Sent Amount
               </div>
-              <div class="stat-value">
-                <ae-amount
-                  :amount="userStats.totalTipAmount"
-                  :round="2"
-                />
-                <fiat-value :amount="userStats.totalTipAmount" />
-              </div>
+              <ae-amount-fiat
+                class="stat-value"
+                :amount="userStats.totalTipAmount"
+              />
             </div>
             <div class="stat">
               <div class="stat-title">
@@ -204,13 +201,10 @@
               <div class="stat-title">
                 Unclaimed Amount
               </div>
-              <div class="stat-value">
-                <ae-amount
-                  :amount="userStats.unclaimedAmount"
-                  :round="2"
-                />
-                <fiat-value :amount="userStats.unclaimedAmount" />
-              </div>
+              <ae-amount-fiat
+                class="stat-value"
+                :amount="userStats.unclaimedAmount"
+              />
             </div>
           </div>
         </div>
@@ -273,8 +267,7 @@ import LeftSection from '../components/layout/LeftSection.vue';
 import RightSection from '../components/layout/RightSection.vue';
 import MobileNavigation from '../components/layout/MobileNavigation.vue';
 import { wallet } from '../utils/walletSearch';
-import FiatValue from '../components/FiatValue.vue';
-import AeAmount from '../components/AeAmount.vue';
+import AeAmountFiat from '../components/AeAmountFiat.vue';
 import Loading from '../components/Loading.vue';
 import { EXPLORER_URL } from '../config/constants';
 import TipsPagination from '../components/TipsPagination.vue';
@@ -287,8 +280,7 @@ export default {
   components: {
     TipsPagination,
     Loading,
-    AeAmount,
-    FiatValue,
+    AeAmountFiat,
     TipComment,
     LeftSection,
     RightSection,
@@ -472,7 +464,8 @@ export default {
         color: $tip_note_color;
       }
 
-      .stat-value {
+      .stat-value,
+      .stat-value /deep/ .currency-value {
         font-size: 0.9rem;
         color: $secondary_color;
         font-weight: 400;
@@ -635,10 +628,6 @@ export default {
 
     & > .row.mobile {
       display: none;
-    }
-
-    .value {
-      color: $secondary_color;
     }
   }
 

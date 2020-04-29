24f76259c13e0f35cb7fa354ecf82740d3f8e2d8
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-04-29 19:04:27 +0300

    Fix lint errors

--- src/components/layout/MobileNavigation.vue
+++ src/components/layout/MobileNavigation.vue
@@ -193,7 +193,7 @@ export default {
 
 @media only screen
   and (min-device-width: 320px)
-    and (max-device-width: 480px)
+  and (max-device-width: 480px)
     and (-webkit-min-device-pixel-ratio: 2) {
   .mobile-navigation {
     width: 100%;

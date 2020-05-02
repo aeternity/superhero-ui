d798655bd1c4b4189ab0bbbc4db1b73cb49c7e52
Milen Radkov <milenradkov@me.com>
2020-05-02 17:01:03 +0300

    refactor privacy locales iteration

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -12,83 +12,16 @@
             {{ $t('views.Privacy.subtitle') }}
           </p>
 
-          <div class="first">
-            <h5><b>{{ $t('views.Privacy.first') }}</b></h5>
-            <p>
-              {{ $t('views.Privacy.first_0') }}
-            </p>
-
-            <p>
-              {{ $t('views.Privacy.first_1') }}
-            </p>
-            <p>
-              {{ $t('views.Privacy.first_2') }}
-            </p>
-          </div>
-          <div class="second">
-            <h5><b>{{ $t('views.Privacy.second') }}</b></h5>
-            <p>{{ $t('views.Privacy.second_0') }}</p>
-            <ul>
-              <li>
-                {{ $t('views.Privacy.second_1') }}
-              </li>
-              <li>
-                {{ $t('views.Privacy.second_2') }}
-              </li>
-              <li>
-                {{ $t('views.Privacy.second_3') }}
-              </li>
-              <li>
-                {{ $t('views.Privacy.second_4') }}
-              </li>
-              <li>
-                {{ $t('views.Privacy.second_5') }}
-              </li>
-            </ul>
-          </div>
-          <div class="third">
-            <h5><b>{{ $t('views.Privacy.third') }}</b></h5>
-            <p>{{ $t('views.Privacy.third_0') }}</p>
-            <ul>
-              <span>{{ $t('views.Privacy.third_1') }}</span>
-              <li>{{ $t('views.Privacy.third_2') }}</li>
-              <li>{{ $t('views.Privacy.third_3') }}</li>
-            </ul>
-          </div>
-          <div class="fourth">
-            <h5><b>{{ $t('views.Privacy.fourth') }}</b></h5>
-            <p>{{ $t('views.Privacy.fourth_0') }}</p>
-            <ul>
-              <li>{{ $t('views.Privacy.fourth_1') }}</li>
-              <li>{{ $t('views.Privacy.fourth_2') }}</li>
-              <li>{{ $t('views.Privacy.fourth_3') }}</li>
-              <li>{{ $t('views.Privacy.fourth_4') }}</li>
-              <li>{{ $t('views.Privacy.fourth_5') }}</li>
-              <li>{{ $t('views.Privacy.fourth_6') }}</li>
-              <li>{{ $t('views.Privacy.fourth_7') }}</li>
-            </ul>
-          </div>
-          <div class="fifth">
-            <h5><b>{{ $t('views.Privacy.fifth') }}</b></h5>
-            <p>{{ $t('views.Privacy.fifth_0') }}</p>
-            <p>{{ $t('views.Privacy.fifth_1') }}</p>
-          </div>
-          <div class="sixth">
-            <h5><b>{{ $t('views.Privacy.sixth') }}</b></h5>
-            <ul>
-              <li>{{ $t('views.Privacy.sixth_0') }}</li>
-              <li>{{ $t('views.Privacy.sixth_1') }}</li>
-              <li>{{ $t('views.Privacy.sixth_2') }}</li>
-            </ul>
-          </div>
-          <div class="seventh">
-            <h5><b>{{ $t('views.Privacy.seventh') }}</b></h5>
-            <p>{{ $t('views.Privacy.seventh_0') }}</p>
-          </div>
-          <div class="eighth">
-            <h5><b>{{ $t('views.Privacy.eighth') }}</b></h5>
-            <p>{{ $t('views.Privacy.eighth_0') }}</p>
-          </div>
+          <template
+            v-for="(item, index) in
+              Object.keys($i18n.messages[$i18n.fallbackLocale].views.Privacy.sections).length"
+          >
+            <i18n
+              :key="index"
+              :path="getI18nPath(index)"
+              :tag="isTitle(index) ? 'h5' : 'p'"
+            />
+          </template>
         </div>
       </div>
     </div>
@@ -106,6 +39,18 @@ export default {
     LeftSection,
     RightSection,
     MobileNavigation,
+  },
+  methods: {
+    getI18nPath(index) {
+      return this.isTitle(index)
+        ? `views.Privacy.sections[${index}].title` : `views.Privacy.sections[${index}].text`;
+    },
+    isTitle(index) {
+      return Object.prototype.hasOwnProperty.call(
+        Object.values(this.$i18n.messages[this.$i18n.fallbackLocale].views.Privacy.sections)[index],
+        'title',
+      );
+    },
   },
 };
 </script>

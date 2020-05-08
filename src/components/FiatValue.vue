0e87b4fd5b9da6c7a4fa49e09b6fa2ed6544ba9b
Valentin Angelov <43376240+vmangelovv@users.noreply.github.com>
2020-05-08 16:33:03 +0300

    Fixes #484 Currency format change

--- src/components/FiatValue.vue
+++ src/components/FiatValue.vue
@@ -3,41 +3,26 @@
     v-if="currencyRates.aeternity"
     class="currency-value"
   >
-    <span v-if="withoutBrackets">
-      <span class="currency-sign">{{ currencySign }}</span>{{ fiatValue }}
-    </span>
-    <span v-else-if="displaySymbol">
-      (~ <span class="currency-sign">{{ currencySign }}</span>{{ fiatValue }})
-    </span>
-    <span v-else>
-      (~ <span class="currency-sign">{{ currencySign }}</span>{{ fiatValue }})
-    </span>
+    <span>(~ {{ fiatValue }})</span>
   </span>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import BigNumber from 'bignumber.js';
-import { currencySigns } from '../utils/util';
 
 export default {
   name: 'FiatValue',
   props: {
     amount: { type: String, required: true },
-    withoutBrackets: { type: Boolean },
-    displaySymbol: { type: Boolean },
   },
   computed: {
     ...mapGetters(['settings', 'currencyRates']),
     fiatValue() {
+      const rate = this.currencyRates.aeternity[this.settings.currency];
       if (!this.amount) {
         return 0;
       }
-      return new BigNumber(this.amount)
-        .multipliedBy(this.currencyRates.aeternity[this.settings.currency]).toFixed(2);
-    },
-    currencySign() {
-      return this.settings && this.settings.currency ? currencySigns[this.settings.currency] : '';
+      return (this.amount * rate).toLocaleString('de-DE', { style: 'currency', currency: this.settings.currency });
     },
   },
 };

3262e6413dbe68c1bcb4be02160ddd467f35d668
Petar Baykov <petbaik26@gmail.com>
2020-05-18 14:24:17 +0300

    New config for avataars. Display avataars for .chain names

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,22 +1,30 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
   <img
-    v-if="!error && avatar"
-    :src="avatar"
+    v-if="!error && profileImage"
+    :src="profileImage"
     loading="lazy"
     @error="error = true"
   >
   <img
-    v-else
+    v-else-if="avatar.type === 'avatar'"
     class="user-identicon"
-    :src="identicon"
+    :src="avatar.src"
   >
+  <div
+    v-else-if="avatar.type === 'identicon'"
+    class="user-identicon"
+    v-html="avatar.src"
+  />
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import jdenticon from 'jdenticon';
 import Avatars from '@dicebear/avatars';
 import sprites from '@dicebear/avatars-avataaars-sprites';
 import Backend from '../utils/backend';
+import { IDENTICON_CONFIG, AVATAR_CONFIG } from '../utils/util';
 
 export default {
   props: {
@@ -29,13 +37,25 @@ export default {
     error: false,
   }),
   computed: {
-    avatar() {
+    ...mapGetters(['chainNames']),
+    profileImage() {
       const userImage = Backend.getProfileImageUrl(this.address);
       return this.address ? userImage : null;
     },
-    identicon() {
-      const avatars = new Avatars(sprites, { eyes: ['happy', 'surprised'], base64: true });
-      return avatars.create(this.address);
+    avatar() {
+      if (this.chainNames[this.address]) {
+        const avatars = new Avatars(sprites, AVATAR_CONFIG);
+        return {
+          type: 'avatar',
+          src: avatars.create(this.chainNames[this.address]),
+        };
+      }
+
+      jdenticon.config = IDENTICON_CONFIG;
+      return {
+        type: 'identicon',
+        src: jdenticon.toSvg(this.address, 32),
+      };
     },
   },
 };

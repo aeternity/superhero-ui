42fefce379471a1c6902eb7084a30e900ff03f46
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-05-26 17:36:38 +1000

    Refactor Mission

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -4,23 +4,21 @@
       <h1>{{ $t('views.Mission.header') }}</h1>
       <hr width="30%">
 
-      <template
-        v-for="(item, index) in
-          Object.keys($i18n.messages[$i18n.fallbackLocale].views.Mission.sections).length"
+      <i18n
+        v-for="(item, index) in $t('views.Mission.sections').length"
+        :key="index"
+        :path="`views.Mission.sections[${index}].text`"
+        tag="p"
       >
-        <i18n
-          :key="index"
-          :path="getI18nPath(index, 'Mission')"
-          :tag="isTitle(index, 'Mission') ? 'h5' : 'p'"
-        >
-          <template v-slot:aeternityBlockchain>
-            <a
-              href="https://aeternity.com/"
-              target="_blank"
-            > {{ $t('views.FAQ.aeternityBlockchain') }} </a>
-          </template>
-        </i18n>
-      </template>
+        <template v-slot:aeternityBlockchain>
+          <a
+            href="https://aeternity.com/"
+            target="_blank"
+          >
+            {{ $t('views.FAQ.aeternityBlockchain') }}
+          </a>
+        </template>
+      </i18n>
       <h1>{{ $t('views.Mission.footer') }}</h1>
       <p>{{ $t('views.Mission.footerContent') }}</p>
     </div>
@@ -35,21 +33,8 @@ export default {
   components: {
     Page,
   },
-  methods: {
-    getI18nPath(index, page) {
-      return this.isTitle(index, page)
-        ? `views.${page}.sections[${index}].title` : `views.${page}.sections[${index}].text`;
-    },
-    isTitle(index, page) {
-      return Object.prototype.hasOwnProperty.call(
-        Object.values(this.$i18n.messages[this.$i18n.fallbackLocale].views[`${page}`].sections)[index],
-        'title',
-      );
-    },
-  },
 };
 </script>
-
 
 <style lang="scss">
   .mission-page {

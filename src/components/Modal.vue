f69dcd61a81738c4a7029a3d0e1204b7a0c30522
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-05-28 13:53:59 +1000

    Extract Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div
+      class="overlay"
+      @click.stop="$emit('close')"
+    />
+    <div class="modal-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.overlay {
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 500;
+}
+
+.modal-content {
+  background-color: transparent;
+  border: 0;
+  position: absolute;
+  z-index: 500;
+}
+</style>

85e740c190069e0e948a010e5a3e21c76c0bb3cf
Ivaylo Badinov <venimus@gmail.com>
2020-05-29 13:41:04 +0300

    Integrate league site as iframe

---
+++ src/views/League.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page>
+    <div class="league-page">
+      <iframe src="https://aeternity.com/league-iframe/index.html" width="100%" />
+    </div>
+  </Page>
+</template>
+<script>
+import Page from '../components/layout/Page.vue';
+
+export default {
+  name: 'League',
+  components: {
+    Page,
+  },
+};
+</script>
+<style lang="scss">
+.league-page iframe {
+  border: 0;
+  height: 99vh;
+  width: 100%;
+}
+</style>

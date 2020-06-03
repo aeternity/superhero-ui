a2ab9a00a2f664c3ce46f4829522c8594b7229e1
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-06-03 16:08:05 +0300

    Added inline svg support

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -13,7 +13,7 @@
       @click="scrollTop"
     >
       <router-link :to="{ name: 'tips' }">
-        <div class="navigation__item__image" />
+        <IconTips class="navigation__item__image" />
         <span>{{ $t('tips') }}</span>
       </router-link>
     </div>
@@ -23,19 +23,19 @@
       @click="scrollTop"
     >
       <router-link :to="{ name: 'user-profile', params: { address: account } }">
-        <div class="navigation__item__image" />
+        <IconUser class="navigation__item__image" />
         <span>{{ $t('components.layout.Navigation.MyProfile') }}</span>
       </router-link>
     </div>
     <div class="navigation__item league">
       <router-link :to="{ name: 'league' }">
-        <div class="navigation__item__image" />
+        <IconDiamond class="navigation__item__image" />
         <span>{{ $t('components.layout.Navigation.League') }}</span>
       </router-link>
     </div>
     <div class="navigation__item faq">
       <router-link :to="{ name: 'faq' }">
-        <div class="navigation__item__image" />
+        <IconHelp class="navigation__item__image" />
         <span>{{ $t('FAQ') }}</span>
       </router-link>
     </div>
@@ -44,9 +44,19 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import IconTips from '../../assets/iconTips.svg?icon-component';
+import IconUser from '../../assets/iconUser.svg?icon-component';
+import IconHelp from '../../assets/iconHelp.svg?icon-component';
+import IconDiamond from '../../assets/iconDiamond.svg?icon-component';
 
 export default {
   name: 'Navigation',
+  components: {
+    IconTips,
+    IconUser,
+    IconHelp,
+    IconDiamond,
+  },
   computed: mapGetters(['account', 'isLoggedIn']),
   methods: {
     scrollTop() {
@@ -97,43 +107,15 @@ export default {
     margin-right: 0.5rem;
     width: 1.3rem;
 
-    .tips & {
-      background-image: url('../../assets/iconTips.svg');
-    }
-
-    .profile & {
-      background-image: url('../../assets/iconUser.svg');
-    }
-
-    .faq & {
-      background-image: url('../../assets/iconHelp.svg');
+    .faq & path,
+    .profile & path,
+    .tips & path,
+    .league & path {
+      fill: currentColor;
     }
 
     .league & {
-      background-image: url('../../assets/iconDiamond.svg');
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-
-    a:hover &,
-    .router-link-exact-active & {
-      .tips & {
-        background-image: url('../../assets/iconTipsActive.svg');
-      }
-
-      .profile & {
-        background-image: url('../../assets/iconUserActive.svg');
-      }
-
-      .faq & {
-        background-image: url('../../assets/iconHelpActive.svg');
-      }
-
-      .league & {
-        background-image: url('../../assets/iconDiamondActive.svg');
-        background-repeat: no-repeat;
-        background-position: center;
-      }
+      padding: 0.2rem;
     }
   }
 
--- vue.config.js
+++ vue.config.js
@@ -14,43 +14,85 @@ module.exports = {
       },
     },
   },
-  chainWebpack: (config) => config
-    .plugin('favicons')
-    .use(FaviconsWebpackPlugin, [{
-      logo: path.resolve(__dirname, 'src/assets/favicon.svg'),
-      mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
-      devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
-      publicPath: '/',
-      favicons: {
-        start_url: '/',
-        appName: 'You Do Not Need A Cape To Be A Hero',
-        appDescription: 'You Do Not Need A Cape To Be A Hero',
-        developerName: 'Aeternity Developers',
-        developerURL: 'https://github.com/kenodressel/aepp-boilerplate',
-        background: '#ff0d6a',
-        theme_color: '#ff0d6a',
-        icons: {
-          coast: false,
-          yandex: false,
-          windows: false,
+  chainWebpack: (config) => {
+    config
+      .plugin('favicons')
+      .use(FaviconsWebpackPlugin, [{
+        logo: path.resolve(__dirname, 'src/assets/favicon.svg'),
+        mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
+        devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
+        publicPath: '/',
+        favicons: {
+          start_url: '/',
+          appName: 'You Do Not Need A Cape To Be A Hero',
+          appDescription: 'You Do Not Need A Cape To Be A Hero',
+          developerName: 'Aeternity Developers',
+          developerURL: 'https://github.com/kenodressel/aepp-boilerplate',
+          background: '#ff0d6a',
+          theme_color: '#ff0d6a',
+          icons: {
+            coast: false,
+            yandex: false,
+            windows: false,
+          },
         },
-      },
-    }])
-    .end()
-    .plugin('copy')
-    .tap((args) => {
-      args[0].push({
-        from: './node_modules/@aeternity/superhero-button/dist',
-        to: './buttons',
-        toType: 'dir',
-      });
-      return args;
-    })
-    .end()
-    .module
-    .rule('aes')
-    .test(/\.aes$/)
-    .use('raw-loader')
-    .loader('raw-loader')
-    .end(),
+      }])
+      .end()
+      .plugin('copy')
+      .tap((args) => {
+        args[0].push({
+          from: './node_modules/@aeternity/superhero-button/dist',
+          to: './buttons',
+          toType: 'dir',
+        });
+        return args;
+      })
+      .end()
+      .module
+      .rule('aes')
+      .test(/\.aes$/)
+      .use('raw-loader')
+      .loader('raw-loader')
+      .end();
+
+    config.module.rule('svg')
+      .uses.clear().end()
+      .oneOf('icon-component')
+      .resourceQuery(/icon-component/)
+      .use('babel-loader')
+      .loader('babel-loader')
+      .options({ configFile: false, presets: ['@babel/preset-env'] })
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [{
+            addClassesToSVGElement: {
+              type: 'full',
+              fn(data, options, extra) {
+                const svg = data.content[0];
+                svg.class.add('icon', path.basename(extra.path, '.svg'));
+                return data;
+              },
+            },
+          }],
+        },
+      })
+      .end()
+      .end()
+      .oneOf('default')
+      .use('svg-url-loader')
+      .loader('svg-url-loader')
+      .options({
+        noquotes: true,
+        limit: 4096,
+        name: 'img/[name].[hash:8].[ext]',
+      })
+      .end()
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .end();
+    return config;
+  },
 };

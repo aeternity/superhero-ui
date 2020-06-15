109a93a18ff92da8d6ee079e26b59df26fe7fb7d
Yury Shapkarin <yury@shapkarin.me>
2020-06-15 12:50:10 +0300

    remove useless code

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -1,162 +1,36 @@
 <template>
   <Page class="league-page-container">
     <div
-      class="league-page"
+      class="conference-page"
     >
-      <template v-if="counter > 0">
-        {{ counter }}
-      </template>
-      <template v-else>
-        <div
-          id="jitsi"
-        />
-      </template>
+      <div
+        id="jitsi"
+      />
     </div>
   </Page>
 </template>
 <script>
 // eslint-disable-next-line import/no-extraneous-dependencies
 import JitsiMeetExternalAPI from 'jitsi';
-import { mapState, mapGetters, mapMutations } from 'vuex';
-import BrowserWindowMessageConnection from '@aeternity/aepp-sdk/es/utils/aepp-wallet-communication/connection/browser-window-message'
 import Page from '../components/layout/Page.vue';
-import Util, { IS_MOBILE_DEVICE, createDeepLinkUrl } from '../utils/util';
-import { client } from '../utils/aeternity';
-
-const TIMEOUT = 4000;
+import { IS_MOBILE_DEVICE } from '../utils/util';
 
 export default {
   name: 'Conference',
   components: {
     Page,
   },
-  data() {
-    return {
-      counter: TIMEOUT / 1000,
-      some: '',
-    };
-  },
-  computed: {
-    ...mapState(['useSdkWallet', 'account']),
-    ...mapGetters(['isLoggedIn']),
-    // todo: later we can use forward from deeplink to main page and redirect to this one
-    ...mapMutations(['setLoggedInAccount']),
-  },
-  async created() {
-
-    const message = `I would like to generate JWT token at ${new Date().toUTCString()}`;
-
-    const communicate = new BrowserWindowMessageConnection();
-
-    communicate.connect(async ({ wallet }) => {
-      if (wallet) {
-        const signature = await client.signMessage(message);
-        const address = client.rpcClient.getCurrentAccount();
-
-        const token = await (await fetch('https://jwt.z52da5wt.xyz/claim ', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ address, message, signature }),
-        })).text();
-        // eslint-disable-next-line no-undef, no-new
-        new JitsiMeetExternalAPI('test.league.aeternity.org', {
-          parentNode: document.querySelector('#jitsi'),
-          width: '100%',
-          height: 440,
-          roomName: this.$route.params.room,
-          jwt: token,
-          configOverwrite: {
-            disableDeepLinking: IS_MOBILE_DEVICE,
-          },
-        });
-      }
+  created() {
+    // eslint-disable-next-line no-new
+    new JitsiMeetExternalAPI('test.league.aeternity.org', {
+      parentNode: document.querySelector('#jitsi'),
+      width: '100%',
+      height: 440,
+      roomName: this.$route.params.room,
+      configOverwrite: {
+        disableDeepLinking: IS_MOBILE_DEVICE,
+      },
     });
-
-    if (false) {
-      if (this.$route.query.fromWallet === 'true') {
-        this.counter = 0;
-      } else {
-        this.runCounter();
-      }
-
-      setTimeout(async () => {
-        const message = `I would like to generate JWT token at ${new Date().toUTCString()}`;
-        let signature;
-        let address;
-        if (this.useSdkWallet) {
-          signature = await client.signMessage(message);
-          address = client.rpcClient.getCurrentAccount();
-
-          const token = await (await fetch('https://jwt.z52da5wt.xyz/claim ', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ address, message, signature }),
-          })).text();
-          // eslint-disable-next-line no-undef, no-new
-          new JitsiMeetExternalAPI('test.league.aeternity.org', {
-            parentNode: document.querySelector('#jitsi'),
-            width: '100%',
-            height: 440,
-            roomName: this.$route.params.room,
-            jwt: token,
-            configOverwrite: {
-              disableDeepLinking: IS_MOBILE_DEVICE,
-            },
-          });
-        } else {
-          // const currentUrl = new URL(window.location);
-          // currentUrl.search = '';
-          // const successUrl = encodeURIComponent(`${currentUrl}?result=success&signature={signature}&fromWallet=true`);
-          // // window.location = createDeepLinkUrl({
-          // window.location = createDeepLinkUrl({
-          //   type: 'sign-message',
-          //   message,
-          //   'x-success': successUrl,
-          // });
-
-          // with deeplink
-          if (!this.isLoggedIn) {
-            // TODO: if not login ask about "do you want to use superhero account"
-            const currentUrl = new URL(window.location);
-            currentUrl.search = '';
-            const signLink = createDeepLinkUrl({
-              type: 'sign-message',
-              message: `I would like to generate JWT token at ${new Date().toUTCString()}`,
-              'x-success': `${currentUrl}?result=success&signature={signature}&fromWallet=true`,
-            });
-
-            window.location = createDeepLinkUrl({
-              type: 'address',
-              'x-success': `${signLink}?address={address}&balance={balance}&result=success`,
-            });
-
-            const { address, balance, signature } = this.$route.query;
-            console.log({ address, balance });
-            this.setLoggedInAccount({
-              account: address,
-              balance: Util.atomsToAe(balance).toFixed(2),
-            });
-          }
-        }
-      }, TIMEOUT);
-    }
-  },
-  methods: {
-    sdkLogin() {
-
-    },
-    deepLinkLogin() {
-
-    },
-    runCounter() {
-      setInterval(() => {
-        this.counter -= 1;
-      }, 1000);
-    },
   },
 };
 </script>

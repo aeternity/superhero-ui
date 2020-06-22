09c1620d61ea073e5925bff236bac9859adbc7bd
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-06-22 18:10:31 +0300

    Resized the transaction font on mobile

--- src/components/tipRecords/TransactionRecord.vue
+++ src/components/tipRecords/TransactionRecord.vue
@@ -70,88 +70,97 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .transaction-record {
-    padding: 1rem;
-    background-color: $light_color;
-    color: $light_font_color;
-    font-size: 0.65rem;
-    font-weight: 400;
-    position: relative;
-    border-bottom: 0.15rem solid $background_color;
+.transaction-record {
+  padding: 1rem;
+  background-color: $light_color;
+  color: $light_font_color;
+  font-size: 0.65rem;
+  font-weight: 400;
+  position: relative;
+  border-bottom: 0.15rem solid $background_color;
+}
+
+.dots-menu {
+  position: absolute;
+  top: 0.25rem;
+  right: 0.75rem;
+}
+
+.transaction-data {
+  line-height: 1rem;
+  margin-bottom: 0.2rem;
+
+  span {
+    font-size: 0.75rem;
+    font-weight: 600;
+    margin-right: 0.3rem;
+    color: $tip_note_color;
   }
 
-  .dots-menu {
-    position: absolute;
-    top: 0.25rem;
-    right: 0.75rem;
+  .address {
+    vertical-align: bottom;
   }
+}
 
-  .transaction-data {
-    line-height: 1rem;
-    margin-bottom: 0.2rem;
+.tip-transaction {
+  flex-grow: 1;
 
-    span {
-      font-size: 0.75rem;
-      font-weight: 600;
-      margin-right: 0.3rem;
-      color: $tip_note_color;
-    }
-
-    .address {
-      vertical-align: bottom;
-    }
-  }
-
-  .tip-transaction {
-    flex-grow: 1;
-
-    &:hover {
-      cursor: pointer;
-    }
-  }
-
-  .transaction-preview {
-    display: flex;
-    margin-bottom: 0.2rem;
-    align-items: center;
-
-    svg {
-      color: $tip_note_color;
-      width: 0.8rem;
-      height: 0.8rem;
-      margin-right: 0.3rem;
-      vertical-align: sub;
-    }
-
-    .link {
-      flex-grow: 1;
-      line-height: 1.2rem;
-    }
-  }
-
-  .actions {
-    display: flex;
-    align-items: center;
-  }
-
-  .view-tip {
-    flex-grow: 1;
-  }
-
-  .date {
-    flex-grow: 1;
-    text-align: right;
-  }
-
-  .preview-image {
-    width: 6.5rem;
-    height: 3.3rem;
-  }
-
-  .link:hover,
-  .transaction-data:hover,
-  .view-tip:hover {
-    filter: brightness(1.3);
+  &:hover {
     cursor: pointer;
   }
+}
+
+.transaction-preview {
+  display: flex;
+  margin-bottom: 0.2rem;
+  align-items: center;
+
+  svg {
+    color: $tip_note_color;
+    width: 0.8rem;
+    height: 0.8rem;
+    margin-right: 0.3rem;
+    vertical-align: sub;
+  }
+
+  .link {
+    flex-grow: 1;
+    line-height: 1.2rem;
+  }
+}
+
+.actions {
+  display: flex;
+  align-items: center;
+}
+
+.view-tip {
+  flex-grow: 1;
+}
+
+.date {
+  flex-grow: 1;
+  text-align: right;
+}
+
+.preview-image {
+  width: 6.5rem;
+  height: 3.3rem;
+}
+
+.link:hover,
+.transaction-data:hover,
+.view-tip:hover {
+  filter: brightness(1.3);
+  cursor: pointer;
+}
+
+@media only screen
+  and (min-device-width: 320px)
+  and (max-device-width: 480px)
+  and (-webkit-min-device-pixel-ratio: 2) {
+  .transaction-data {
+    font-size: 0.55rem;
+  }
+}
 </style>

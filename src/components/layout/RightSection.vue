da45b20abe54212b9ceed6e18331948d4ecb1518
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-06-24 17:36:04 +1000

    RightSection: Add shrink button

--- src/components/layout/RightSection.vue
+++ src/components/layout/RightSection.vue
@@ -5,185 +5,46 @@
       sided
     />
 
-    <div class="section wallet">
-      <h2>
-        <img src="../../assets/iconWallet.svg">
-        {{ $t('components.layout.RightSection.Wallet') }}
-      </h2>
+    <RightSectionWallet
+      class="section"
+      :open="!showTopics"
+      @toggle="showTopics = !showTopics"
+    />
 
-      <iframe
-        v-if="useIframeWallet"
-        :src="walletUrl"
-      />
-      <template v-else-if="isLoggedIn">
-        <div class="account">
-          {{ account }}
-        </div>
-        <div class="not-bootstrap-row">
-          <AeAmount :amount="balance" />
-          <Dropdown
-            v-if="currencyDropdownOptions"
-            :options="currencyDropdownOptions"
-            :method="updateCurrency"
-            :selected="selectedCurrency"
-          />
-        </div>
-      </template>
-    </div>
-
-    <div class="section trending">
-      <h2>
-        <img src="../../assets/iconTrending.svg">
-        {{ $t('components.layout.RightSection.Trending') }}
-      </h2>
-
-      <div class="topics-list">
-        <div
-          v-for="[topic, data] in topics"
-          :key="topic"
-          class="not-bootstrap-row"
-        >
-          <Topic
-            class="text-ellipsis"
-            :topic="topic"
-          />
-          <AeAmountFiat :amount="data.amount" />
-        </div>
-      </div>
-    </div>
+    <RightSectionTopics
+      class="section"
+      :open="showTopics"
+      @toggle="showTopics = !showTopics"
+    />
 
     <FooterSection />
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapGetters } from 'vuex';
-import BigNumber from 'bignumber.js';
-import AeAmount from '../AeAmount.vue';
-import AeAmountFiat from '../AeAmountFiat.vue';
-import Topic from '../tipRecords/Topic.vue';
 import FooterSection from './FooterSection.vue';
-import Dropdown from '../Dropdown.vue';
 import SearchInput from './SearchInput.vue';
+import RightSectionWallet from './RightSectionWallet.vue';
+import RightSectionTopics from './RightSectionTopics.vue';
 
 export default {
   name: 'RightSection',
   components: {
-    Topic,
-    AeAmount,
-    AeAmountFiat,
-    FooterSection,
-    Dropdown,
     SearchInput,
+    RightSectionWallet,
+    RightSectionTopics,
+    FooterSection,
   },
   data: () => ({
-    walletUrl: process.env.VUE_APP_WALLET_URL,
+    showTopics: false,
   }),
-  computed: {
-    ...mapGetters(['isLoggedIn']),
-    ...mapState(['balance', 'account', 'useIframeWallet']),
-    ...mapState({
-      topics: ({ topics }) => topics.filter(([t]) => t !== '#test'),
-      selectedCurrency: ({ settings }) => settings.currency,
-      currencyDropdownOptions({ currencyRates: { aeternity } = {}, balance }) {
-        if (!aeternity || !balance) return null;
-        return Object.keys(aeternity).map((currency) => ({
-          text: [
-            new BigNumber(balance).multipliedBy(aeternity[currency]).toFixed(2),
-            currency.toUpperCase(),
-          ].join(' '),
-          value: currency,
-        }));
-      },
-    }),
-  },
-  methods: mapMutations(['updateCurrency']),
 };
 </script>
 
 <style lang="scss" scoped>
-.right-section {
-  color: $light_font_color;
-  font-size: 0.75rem;
-
-  .section {
-    background-color: $article_content_color;
-    border-radius: 0.5rem;
-    padding: 0.8rem 1rem 1rem 1rem;
-    margin-bottom: 0.5rem;
-
-    h2 {
-      color: $standard_font_color;
-      font-size: 1rem;
-      font-weight: 600;
-      padding-bottom: 0.65rem;
-
-      img {
-        height: 1.2rem;
-        margin-right: 0.2rem;
-      }
-    }
-
-    .ae-amount .ae {
-      color: $secondary_color;
-    }
-
-    .not-bootstrap-row {
-      display: flex;
-      align-items: center;
-
-      > :nth-child(1) {
-        flex-grow: 1;
-      }
-    }
-
-    &.search-input {
-      padding: 0;
-    }
-
-    &.wallet {
-      iframe {
-        border: none;
-        width: 100%;
-        height: 657px;
-      }
-
-      .account {
-        color: $light_font_color;
-        font-size: 0.52rem;
-        position: relative;
-        top: -0.5rem;
-      }
-
-      .ae-amount {
-        font-size: 1.3rem;
-        color: $standard_font_color;
-      }
-    }
-
-    &.trending .topics-list {
-      transition: max-height 0.25s ease-in;
-      -ms-overflow-style: none;
-      overflow-y: auto;
-      scrollbar-width: none;
-      max-height: 25rem;
-
-      &::-webkit-scrollbar {
-        display: none;
-      }
-
-      @media (max-height: 650px) {
-        max-height: 10rem;
-      }
-
-      .not-bootstrap-row {
-        margin: 0.5rem 0;
-
-        .ae-amount-fiat {
-          flex-shrink: 0;
-        }
-      }
-    }
-  }
+.right-section .section {
+  background-color: $article_content_color;
+  border-radius: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 </style>
---
+++ src/components/layout/RightSectionTitle.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="right-section-title">
+    <span><slot /></span>
+
+    <button
+      v-if="useIframeWallet"
+      @click="$emit('toggle')"
+    >
+      <IconExpanded :class="{ open }" />
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import IconExpanded from '../../assets/iconExpanded.svg?icon-component';
+
+export default {
+  components: { IconExpanded },
+  computed: mapState(['useIframeWallet']),
+  props: { open: Boolean },
+};
+</script>
+
+<style lang="scss" scoped>
+.right-section-title {
+  color: $standard_font_color;
+  font-weight: 600;
+  padding-bottom: 0.65rem;
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    height: 1.2rem;
+    margin-right: 0.2rem;
+  }
+
+  button {
+    color: $light_font_color;
+    background: none;
+    border: none;
+    outline: none;
+    padding: 0;
+
+    &:hover {
+      color: $lighter_font_color;
+    }
+
+    svg {
+      height: 1em;
+      transform: rotate(180deg);
+
+      &.open {
+        transform: rotate(0);
+      }
+    }
+  }
+}
+</style>

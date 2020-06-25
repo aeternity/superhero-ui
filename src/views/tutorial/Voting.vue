96dc64e5668ccf363c62ba0a71d92cd30c9392d8
Milen Radkov <milenradkov@me.com>
2020-06-25 16:06:04 +0300

    add turorial views

---
+++ src/views/tutorial/Voting.vue
@@ -0,0 +1,26 @@
+<template>
+  <Page>
+    <div class="tutorial-page">
+      <h1>{{ $t('views.Voting.header') }}</h1>
+      <hr width="30%">
+
+      <i18n
+        v-for="(item, index) in $t('views.Voting.sections').length"
+        :key="index"
+        :path="`views.Voting.sections[${index}].text`"
+        tag="p"
+      />
+    </div>
+  </Page>
+</template>
+
+<script>
+import Page from '../../components/layout/Page.vue';
+
+export default {
+  name: 'Voting',
+  components: {
+    Page,
+  },
+};
+</script>

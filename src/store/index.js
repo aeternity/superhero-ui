8e66d411672c67499c0bceb9d03e58815561dc0c
Philipp Piwo <github@piwo.space>
2020-07-02 10:03:32 +0200

    fixed token balances vuex usage

--- src/store/index.js
+++ src/store/index.js
@@ -27,7 +27,7 @@ export default new Vuex.Store({
     verifiedUrls: [],
     graylistedUrls: [],
     tokenInfo: {},
-    tokenBalances: {},
+    tokenBalances: [],
     isHiddenContent: true,
     useSdkWallet: false,
     useIframeWallet: false,

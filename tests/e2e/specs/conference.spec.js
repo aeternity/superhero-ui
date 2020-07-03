47385d89e3ff4ee0eff5c9283b797aeb56d45358
Yury Shapkarin <yury@shapkarin.me>
2020-07-04 02:49:43 +0300

    add encoded symbols to test's ROOM_NAME

--- tests/e2e/specs/conference.spec.js
+++ tests/e2e/specs/conference.spec.js
@@ -1,6 +1,5 @@
 describe('Conference.vue', () => {
-  // todo: improve jitis regexp to exclude <>{}^! and so on
-  const ROOM_NAME = 'SOME_VALID_ROOM_NAME_1234567890$()[]-+';
+  const ROOM_NAME = 'SOME_VALID_ROOM_NAME_1234567890$()[]-+{><}^';
   // the same as we have at jitsi create room form and at https://github.com/aeternity/jitsi-meet/blob/80463517e9216cbb2e51d08bb69f5477af005bdb/react/features/welcome/components/WelcomePage.web.js#L23
   const isValid = ROOM_NAME.match(/^[^?&:\u0022\u0027%#]+$/);
 

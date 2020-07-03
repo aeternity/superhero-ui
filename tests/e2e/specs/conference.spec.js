92247ac1b6ed2c6a852f579d387945e72f87430b
Yury Shapkarin <yury@shapkarin.me>
2020-07-03 16:07:15 +0300

    test set correct iframe path by Vue route [e2e without chromeWebSecurity: true]

--- tests/e2e/specs/conference.spec.js
+++ tests/e2e/specs/conference.spec.js
@@ -1,24 +1,21 @@
-// about cypress and iframes https://www.cypress.io/blog/2020/02/12/working-with-iframes-in-cypress/
-const getIframeDocument = () => cy
-  .get('#jitsiConferenceFrame0')
-  .its('0.contentDocument')
-  .should('exist');
-
-const getIframeBody = () => getIframeDocument()
-  .its('body').should('not.be.undefined')
-  .then(cy.wrap);
-
 describe('Conference.vue', () => {
+  const ROOM = 'SOME_ROOM_NAME';
+  const isValidName = ROOM.match(/^[^?&:"'%#]+$/);
+  // not sure how to read .env
+  const DOMAIN = 'test.league.aeternity.org';
+
   before(() => {
-    cy.visit('/league');
+    if (isValidName) {
+      cy.visit(`/league/${ROOM}`);
+    }
   });
 
-  it('change route by click to the iframe "OK" button', () => {
-    const ROOM = 'SOME_ROOM_NAME';
-
-    getIframeBody().find('#enter_room_field').type(ROOM);
-
-    getIframeBody().find('#enter_room_button').click();
-    cy.location('pathname').should('eq', `/league/${ROOM}`);
+  it('set the correct iframe path by Vue route', () => {
+    if (isValidName) {
+      cy.get('#jitsiConferenceFrame0')
+        .should('exist')
+        .should('have.attr', 'src')
+        .and('contains', `https://${DOMAIN}/${ROOM}`);
+    }
   });
 });

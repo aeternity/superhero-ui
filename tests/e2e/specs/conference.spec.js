dcb1fc89198677a70d9731e028fac807b7e6cb7a
Yury Shapkarin <yury@shapkarin.me>
2020-07-04 00:37:18 +0300

    Conference.vue test comments and fix error typo

--- tests/e2e/specs/conference.spec.js
+++ tests/e2e/specs/conference.spec.js
@@ -1,11 +1,12 @@
 describe('Conference.vue', () => {
-  const ROOM_NAME = 'SOME_ROOM_NAME';
-  // the same as we have at jitsi create room form
-  const isValid = ROOM_NAME.match(/^[^?&:"'%#]+$/);
+  // todo: improve jitis regexp to exclude <>{}^! and so on
+  const ROOM_NAME = 'SOME_VALID_ROOM_NAME_1234567890$()[]-+';
+  // the same as we have at jitsi create room form and at https://github.com/aeternity/jitsi-meet/blob/80463517e9216cbb2e51d08bb69f5477af005bdb/react/features/welcome/components/WelcomePage.web.js#L23
+  const isValid = ROOM_NAME.match(/^[^?&:\u0022\u0027%#]+$/);
 
   before(() => {
     if (!isValid) {
-      throw new Error('const ROOM_NAME is not not valid');
+      throw new Error('const ROOM_NAME is not valid');
     }
     cy.visit(`/league/${ROOM_NAME}`);
   });

42a1af18c256053b23e8c0fed63e96756c7ac8ce
Yury Shapkarin <yury@shapkarin.me>
2020-07-03 18:14:58 +0300

    refact: second error will never be called due before() call

--- tests/e2e/specs/conference.spec.js
+++ tests/e2e/specs/conference.spec.js
@@ -2,27 +2,19 @@ describe('Conference.vue', () => {
   const ROOM = 'SOME_ROOM_NAME';
   // the same as we have at jitsi create room form
   const isValidName = ROOM.match(/^[^?&:"'%#]+$/);
-  // not sure how to read .env
-  const DOMAIN = 'test.league.aeternity.org';
-
-  const INCOME_DATA_ERROR = 'const ROOM is not not valid';
 
   before(() => {
     if (isValidName) {
       cy.visit(`/league/${ROOM}`);
     } else {
-      throw new Error(INCOME_DATA_ERROR);
+      throw new Error('const ROOM is not not valid');
     }
   });
 
   it('set the correct iframe path by Vue route', () => {
-    if (isValidName) {
-      cy.get('#jitsiConferenceFrame0')
-        .should('exist')
-        .should('have.attr', 'src')
-        .should('contains', `https://${DOMAIN}/${ROOM}`);
-    } else {
-      throw new Error(INCOME_DATA_ERROR);
-    }
+    cy.get('#jitsiConferenceFrame0')
+      .should('exist')
+      .should('have.attr', 'src')
+      .should('contains', ROOM);
   });
 });

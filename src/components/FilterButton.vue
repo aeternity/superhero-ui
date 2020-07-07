13bd492ead30b747241876a915bcc16cd006a8b2
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-07-07 17:33:40 +0300

    Removed filter-button styles into a separate component

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="filter-button"
+    @click="$emit('click', $event)"
+  >
+    <template>
+      <slot />
+    </template>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.filter-button {
+  border-radius: 0.9rem;
+  font-size: 0.8rem;
+  font-weight: 600;
+  color: $light_font_color;
+  display: inline-block;
+  padding: 0.45rem 0.9rem;
+
+  &.active {
+    color: $custom_links_color;
+    background-color: $thumbnail_background_color;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>

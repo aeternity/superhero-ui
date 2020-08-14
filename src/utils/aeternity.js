9375a4c74a723ed768e95f36ef7cda04084c7967
Philipp Piwo <github@piwo.space>
2020-08-14 17:11:50 +0200

    prepare for simultaneous v1, v2 contract use

--- src/utils/aeternity.js
+++ src/utils/aeternity.js
@@ -17,7 +17,7 @@ const nodeUrlTestNet = 'https://testnet.aeternity.io';
 const compilerUrl = 'https://latest.compiler.aepps.com';
 const contractAddress = window.Cypress
   ? 'ct_2GRP3xp7KWrKtZSnYfdcLnreRWrntWf5aTsxtLqpBHp71EFc3i'
-  : 'ct_n35Ufg9LEstVLQb3ov1jjHeMmJvFqjewp9GF3sYTWqaebFUWk';
+  : 'ct_217sVfSyoAQHPsrRQEVEpw4hiiqVaQro7SvcoYt9pG9kzTyV6r';
 let contract;
 
 export let client; // eslint-disable-line import/no-mutable-exports

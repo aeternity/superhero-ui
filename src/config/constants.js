ecc4dccfc87dd24e3d5ed422dee859bca541a61e
Philipp Piwo <github@piwo.space>
2020-08-19 17:16:30 +0200

    update to hosted backend

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = typeof Cypress !== 'undefined' ? 'https://testnet.superhero.aeternity.art' : 'http://localhost:3000';
+export const BACKEND_URL = typeof Cypress !== 'undefined' ? 'https://testnet.superhero.aeternity.art' : 'https://test-tipping.aeternity.art';
 export const MIDDLEWARE_URL = 'https://testnet.aeternity.io/';
 export const EXPLORER_URL = 'https://testnet.aeternal.io/';

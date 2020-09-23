3471507941f7065805db7ce1ce2a22dbfe896596
Milen Radkov <milenradkov@me.com>
2020-09-23 16:25:30 +0300

    fix I18n casing across different views

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -5,7 +5,7 @@
       {{ $t('views.Terms.subtitle') }}
     </p>
 
-    <I18n
+    <i18n
       v-for="(item, index) in $t('views.Terms.sections').length"
       :key="index"
       :path="getI18nPath(index, 'Terms')"

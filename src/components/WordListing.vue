c3a50faad9a476ffd39afc86389dc58bb2dda6ca
Philipp Piwo <github@piwo.space>
2020-09-25 15:18:50 +0200

    style adjustments

--- src/components/WordListing.vue
+++ src/components/WordListing.vue
@@ -8,7 +8,7 @@
         {{ word }}
       </RouterLink>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-3">
       <AeAmount
         v-if="buyPrice"
         :amount="buyPrice"
@@ -18,8 +18,11 @@
         :small="true"
       />
     </div>
-    <div class="col-md-2">
-      <span v-if="totalSupply">{{ totalSupply }}</span>
+    <div class="col-md-3">
+      <AeAmount
+        v-if="totalSupply !== null && tokenAddress"
+        :amount="totalSupply"
+        :token="tokenAddress" />
       <Loading
         v-else
         :small="true"
@@ -48,9 +51,12 @@
       v-if="showBuyModal"
       @close="showBuyModal = false"
     >
-      <div>Account Balance</div>
-      <AeAmountFiat :amount="balance" />
-      <div class="mt-3">Amount buying</div>
+      <div class="label">Account Balance</div>
+      <AeAmount
+        :amount="tokenBalance"
+        :token="tokenAddress"
+      />
+      <div class="mt-3 label">Amount buying</div>
       <div class="input-group mb-2">
         <input
           v-model="buyAmount"
@@ -59,9 +65,9 @@
           class="form-control"
         >
       </div>
-      <div class="mt-3">Total you pay</div>
+      <div class="mt-3 label">Total you pay</div>
       <AeAmountFiat :amount="buyAmount * buyPrice"/>
-      <div class="mt-3">
+      <div class="mt-3 text-center">
         <OutlinedButton
           class="green"
           @click="buy"
@@ -76,9 +82,12 @@
       v-if="showSellModal"
       @close="showSellModal = false"
     >
-      <div>Account Balance</div>
-      <AeAmountFiat :amount="balance" />
-      <div class="mt-3">Amount selling</div>
+      <div class="label">Account Balance</div>
+      <AeAmount
+        :amount="tokenBalance"
+        :token="tokenAddress"
+      />
+      <div class="mt-3 label">Amount selling</div>
       <div class="input-group mb-2">
         <input
           v-model="sellAmount"
@@ -87,9 +96,9 @@
           class="form-control"
         >
       </div>
-      <div class="mt-3">Total you get</div>
+      <div class="mt-3 label">Total you get</div>
       <AeAmountFiat :amount="sellAmount * sellPrice"/>
-      <div class="mt-3">
+      <div class="mt-3 text-center">
         <OutlinedButton
           class="red"
           @click="sell"
@@ -130,67 +139,72 @@ export default {
     sale: { type: String, required: true },
   },
   data: () => ({
+    contract: null,
     buyPrice: null,
     sellPrice: null,
     buyAmount: 0,
     sellAmount: 0,
     spread: 0,
     totalSupply: null,
+    tokenAddress: null,
     showBuyModal: false,
     showSellModal: false,
   }),
   computed: {
-    ...mapState(['address', 'balance']),
+    ...mapState(['address', 'balance', 'tokenBalances']),
+    tokenBalance() {
+      const balance = this.tokenBalances && this.tokenAddress
+        && this.tokenBalances.find((t) => t.token === this.tokenAddress);
+
+      return balance ? balance.balance : '0';
+    },
   },
   mounted() {
-    this.selectWord(this.word, this.sale);
+    this.loadWordData();
   },
   methods: {
-    async selectWord(word, sale) {
-      this.selectedWordContract = await client
-        .getContractInstance(TOKEN_SALE_CONTRACT, { contractAddress: sale });
+    async loadWordData() {
+      this.contract = this.contract ? this.contract : await client
+        .getContractInstance(TOKEN_SALE_CONTRACT, { contractAddress: this.sale });
       this.spread = util.shiftDecimalPlaces(
-        (await this.selectedWordContract.methods.spread()).decodedResult, -18,
+        (await this.contract.methods.spread()).decodedResult, -18,
       ).toFixed();
 
-      const tokenAddress = (await this.selectedWordContract.methods.get_token()).decodedResult;
+      this.tokenAddress = (await this.contract.methods.get_token()).decodedResult;
       const tokenContract = await client
-        .getContractInstance(FUNGIBLE_TOKEN_CONTRACT, { contractAddress: tokenAddress });
+        .getContractInstance(FUNGIBLE_TOKEN_CONTRACT, { contractAddress: this.tokenAddress });
 
-      this.totalSupply =
-        util.shiftDecimalPlaces(
-          (await tokenContract.methods.total_supply()).decodedResult, -18,
-        ).toFixed();
+      this.totalSupply = (await tokenContract.methods.total_supply()).decodedResult;
 
-      const [buy, sell] = (await this.selectedWordContract.methods.prices()).decodedResult;
+      const [buy, sell] = (await this.contract.methods.prices()).decodedResult;
       this.buyPrice = 1 / buy;
       this.sellPrice = 1 / sell;
     },
     async buy() {
-      await this.selectedWordContract.methods
+      await this.contract.methods
         .buy({ amount: util.shiftDecimalPlaces(this.buyAmount, 18).toFixed() });
-      const token = (await this.selectedWordContract.methods.get_token()).decodedResult;
+      const token = (await this.contract.methods.get_token()).decodedResult;
       await backend.invalidateTokenCache(token);
       EventBus.$emit('reloadData');
-      await this.selectWord(this.word, this.sale);
+      this.loadWordData();
     },
     async sell() {
       const amount = util.shiftDecimalPlaces(this.sellAmount, 18).toFixed();
-      const token = (await this.selectedWordContract.methods.get_token()).decodedResult;
+      const token = (await this.contract.methods.get_token()).decodedResult;
       await createOrChangeAllowance(token, amount,
-        this.selectedWordContract.deployInfo.address.replace('ct_', 'ak_'));
-      await this.selectedWordContract.methods.sell(amount);
+        this.contract.deployInfo.address.replace('ct_', 'ak_'));
+      await this.contract.methods.sell(amount);
       EventBus.$emit('reloadData');
-      await this.selectWord(this.word, this.sale);
+      this.loadWordData();
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-a {
-  margin-right: 0.5rem;
-  text-decoration: underline !important;
+.link {
+  color: $secondary_color;
+  text-decoration: none;
 }
 
 h2 {
@@ -206,6 +220,17 @@ h2 {
   @include smallest {
     min-width: 16rem;
     padding: 0.5rem;
+  }
+}
+
+.label {
+  color: $light_font_color;
+  font-size: 0.7rem;
+  margin-bottom: 0.5rem;
+
+  @include desktop {
+    margin-bottom: 0.4rem;
+    line-height: 0.9rem;
   }
 }
 </style>

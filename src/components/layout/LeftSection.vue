6919ccca738f31deb28fe80a5e92c9cce4882c5c
Milen Radkov <milenradkov@me.com>
2020-11-25 10:47:34 +0200

    LeftSection: cleanup unused styles

--- src/components/layout/LeftSection.vue
+++ src/components/layout/LeftSection.vue
@@ -16,20 +16,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.left-section .powered-by {
-  color: $standard_font_color;
-  font-size: 0.7rem;
-
-  a {
-    &:hover {
-      filter: brightness(1.3);
-    }
-
-    img {
-      height: 0.55rem;
-    }
-  }
-}
-</style>

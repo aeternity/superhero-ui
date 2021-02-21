e2c4c96b845e6b90fea725c7c82779136f062c48
Atanas Krondev <krondev@gmail.com>
2021-02-21 20:56:49 +0200

    Use Swagger to access the middleware

--- src/utils/middleware.js
+++
@@ -1,9 +0,0 @@
-import { wrapTry } from './index';
-
-const backendFetch = (path, ...args) => wrapTry(
-  fetch(`${process.env.VUE_APP_MIDDLEWARE_URL}/${path}`, ...args).catch((err) => console.error(err)),
-);
-
-export default class Middleware {
-  static getAex9Balance = async (token, account) => backendFetch(`aex9/balance/${token}/${account}?top=true`);
-}

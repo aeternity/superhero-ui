adcbf5355d2ca53a1cb853ea45f167c75122fc83
Philipp Piwo <github@piwo.space>
2021-04-02 19:05:54 +0200

    await based on id

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,8 @@ import { shiftDecimalPlaces } from '../utils';
 
 export default {
   roundedTokenAmount: ({ tokenInfo }) => (amount, token, round = 2) => {
-    const shiftedAmount = shiftDecimalPlaces(amount, -(tokenInfo[token] ? tokenInfo[token].decimals : 18));
+    const shiftedAmount = shiftDecimalPlaces(amount,
+      -(tokenInfo[token] ? tokenInfo[token].decimals : 18));
     return new BigNumber(shiftedAmount).toFixed(round);
   },
   isLoggedIn: (state) => !!state.address,

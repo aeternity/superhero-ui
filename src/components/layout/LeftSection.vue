89b4045716c7dbfcb4bf2d2b2703ab09f6089b40
Atanas Krondev <krondev@gmail.com>
2021-04-12 16:10:34 +0300

    Remove statistics from left section

--- src/components/layout/LeftSection.vue
+++ src/components/layout/LeftSection.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="left-section">
     <Navigation />
-    <Overview />
   </div>
 </template>
 
 <script>
 import Navigation from './Navigation.vue';
-import Overview from './Overview.vue';
 
 export default {
   components: {
-    Overview,
     Navigation,
   },
 };

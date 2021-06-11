454709b444d628c892614151cc32edcad00c61ec
Atanas Krondev <krondev@gmail.com>
2021-06-11 11:08:41 +0300

    Add Topics on mobile

---
+++ src/views/Trending.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="trending">
+    <BackButtonRibbon>
+      <template #title>
+        {{ $t('Trending topics') }}
+      </template>
+    </BackButtonRibbon>
+    <TopicList />
+  </div>
+</template>
+
+<script>
+import BackButtonRibbon from '../components/BackButtonRibbon.vue';
+import TopicList from '../components/TopicList.vue';
+
+export default {
+  components: { BackButtonRibbon, TopicList },
+};
+</script>
+
+<style lang="scss" scoped>
+.trending {
+  .actions-ribbon {
+    display: flex;
+
+    ::v-deep .title {
+      white-space: nowrap;
+    }
+  }
+
+  .topic-list {
+    padding: 0 8px;
+  }
+}
+</style>

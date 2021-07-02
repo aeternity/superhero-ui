a453b53f282e2cba521bad8df2c49346ebabe6ab
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-07-02 08:21:18 +0300

    feat(ssr): setup custom internal error page

--- server.config.js
+++ server.config.js
@@ -1,6 +1,10 @@
+import path from 'path';
+
 export default {
   plugins: [
-    '@uvue/server/plugins/serverError',
+    ['@uvue/server/plugins/serverError', {
+      path: path.join(__dirname, 'public', '500-error.html'),
+    }],
     '@uvue/server/plugins/static',
   ],
   externalsWhitelist: ['lodash-es', /^@aeternity\/aepp-sdk\/es/],
--- uvue.config.js
+++ uvue.config.js
@@ -1,7 +1,6 @@
 export default {
   plugins: [
     '@uvue/core/plugins/prefetch',
-    '@uvue/core/plugins/errorHandler',
     '../../src/utils/uvueVuexPlugin',
   ],
   paths: {
